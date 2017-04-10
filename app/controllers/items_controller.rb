5f8a825ed01c5a4e54abdaa09f95829c10cc73ed
Sahana Murthy <s.murthy730.com>
2017-04-10 15:49:59 -0700

    Items controller and basic routes created

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,45 @@
 class ItemsController < ApplicationController
+
+  def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to items_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to item_path
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to items_path
+  end
+
+  private
+  def item_params
+    return params.require(:item).permit(:category, :title, :created_by, :published, :description)
+  end
+
 end
