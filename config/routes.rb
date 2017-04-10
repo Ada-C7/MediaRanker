06f14e222647b7480b0f4512b5579f4694f615b1
Tamiko Terada <tamikodevo@gmail.com>
2017-04-10 16:40:55 -0700

    routes added for works

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/works', to: 'works#index', as: 'works'
+  get '/works/new', to: 'works#new', as: 'new_work'
+  post '/works', to: 'works#create'
+  get '/works/:id', to: 'works#show', as: 'work'
+  get '/works/:id/edit', to: 'works#edit', as: 'edit_work'
+  patch '/works/:id', to: 'works#update'
+  put '/works/:id', to: 'works#update'
+  delete '/works/:id', to: 'works#destroy'
 end
