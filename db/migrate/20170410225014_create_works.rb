18b78aa0b4df65806537c6c176bec721f14b055a
Tamiko Terada <tamikodevo@gmail.com>
2017-04-10 16:01:41 -0700

    create Work model and migrate

---
+++ db/migrate/20170410225014_create_works.rb
@@ -0,0 +1,13 @@
+class CreateWorks < ActiveRecord::Migration[5.0]
+  def change
+    create_table :works do |t|
+      t.string :title
+      t.string :by
+      t.integer :year
+      t.text :description
+      t.text :media_type
+
+      t.timestamps
+    end
+  end
+end
