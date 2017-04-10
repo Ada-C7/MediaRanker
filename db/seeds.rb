47091b9c30ce4c7b4790937eb090c960034f9926
Sahana Murthy <s.murthy730.com>
2017-04-10 16:29:03 -0700

    db seeded into item model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+csv_text = File.read(Rails.root.join('db', 'media_seeds.csv' ))
+csv = CSV.parse(csv_text, :headers => true)
+  csv.each do |row|
+    t = Item.new
+    t.category = row[0]
+    t.title = row[1]
+    t.creator = row[2]
+    t.publication_year = row[3]
+    t.description = row[4]
+    t.save
+  end
