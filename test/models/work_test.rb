14297bcd9543da4d62d792a705fbbef76ad858f9
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-04-10 16:30:20 -0700

    generated work model and works table - wrote test for work validations, added validations and tests are passing

---
+++ test/models/work_test.rb
@@ -0,0 +1,60 @@
+require "test_helper"
+
+describe Work do
+
+  describe 'validations' do
+
+    let(:work) { Work.new(title: "Finding Nemo",
+                          creator: "Pixar",
+                          publication_year: "2004",
+                          description: "Dad has to safe son after he has been kidnapped") }
+
+    let(:work_no_title) { Work.new(
+                          creator: "Pixar",
+                          publication_year: "2004",
+                          description: "Dad has to safe son after he has been kidnapped") }
+
+    let(:work_no_creator) { Work.new(title: "Finding Nemo",
+                          publication_year: "2004",
+                          description: "Dad has to safe son after he has been kidnapped") }
+
+    let(:work_non_number_year) { Work.new(title: "Finding Nemo",
+                          creator: "Pixar",
+                          publication_year: "this is not a number",
+                          description: "Dad has to safe son after he has been kidnapped")}
+
+    let(:work_no_attributes) { Work.new }
+
+
+    it 'can be created with all attributes' do
+      results = work.valid?
+      results.must_equal true
+    end
+
+    it 'will not create when given no attributes' do
+        work_no_attributes.valid?.must_equal false
+        work_no_attributes.errors.messages.must_include :title
+        work_no_attributes.errors.messages.must_include :creator
+        work_no_attributes.errors.messages.must_include :publication_year
+    end
+
+    # this is the only validation that gets an error message on the example site
+    it 'must have title' do
+      work_no_title.valid?.must_equal false
+      work_no_title.errors.messages.must_include :title
+    end
+
+    it 'must have a creator' do
+      work_no_creator.valid?.must_equal false
+      work_no_creator.errors.messages.must_include :creator
+    end
+
+    it 'the publication_year has to be an integer' do
+      work_non_number_year.valid?.must_equal false
+      work_non_number_year.errors.messages.must_include :publication_year
+    end
+  end
+
+
+
+end
