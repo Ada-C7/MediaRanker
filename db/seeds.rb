af162ab8870d2f96f1a39c830adff8dfe2716415
Hyunji Kim <ricecakemonster@gmail.com>
2017-04-10 16:51:35 -0700

    Imported csv file. rails db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'csv'
+
+work_csv_text = File.read(Rails.root.join('db', 'media_seeds.csv'))
+work_csv = CSV.parse(work_csv_text, headers: true)
+work_csv.each do |row|
+    t = Work.new
+    t.category = row['category']
+    t.title = row['title']
+    t.creator = row['creator']
+    t.publication_year = row['publication_year']
+    t.description = row['description']
+    t.save
+end
