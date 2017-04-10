bdb10b49cce27d59cf48dfdf4afa6e191d718db7
Danielle Birbal <birbalds@gmail.com>
2017-04-10 15:55:13 -0700

    Added user controller + methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    root 'home#index', as: 'home'
+
+    resources :users, only: [:new, :show, :index, :create]
+    resources :works
+    resources :votes, only: [:new, :create]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+work_csv_text = File.read(Rails.root.join('lib', 'seeds', 'media_seeds.csv'))
+work_csv = CSV.parse(work_csv_text, headers: true)
+
+work_csv.each do |row|
+    w = Work.new
+    w.type = row['category']
+    w.title = row['title']
+    w.creator = row['creator']
+    w.published_year = row['published_year']
+    w.description = row['description']
+    w.save
+end
