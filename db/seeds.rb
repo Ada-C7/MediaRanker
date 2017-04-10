3b35834c492667965b838e54faada65642f1195b
Marisol Lopez <lmarisolopez@gmail.com>
2017-04-10 16:42:34 -0700

    created new folder for csv file, wrote code to seed csv file into db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+media_text = File.read(Rails.root.join('lib', 'seeds', 'media_seeds.csv'))
+csv = CSV.parse(media_text, :headers => true)
+csv.each do |row|
+  t = Work.new
+  t.category = row[0]
+  t.title = row[1]
+  t.creator = row[2]
+  t.publication_year = row[3]
+  t.description = row[4]
+  t.save
+end
