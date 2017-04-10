fb9b86605bfc58355b44608d1ec3193ee8c9a8d3
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-04-10 16:51:38 -0700

    added one view to check routes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,50 +1,17 @@
 class VotesController < ApplicationController
 
-  def index
-    @votes = Vote.all
-  end
-
-  def new
-    @vote = Vote.new
-  end
-
   def create
     @vote = Vote.new
-    @vote.user_id = params[:user][:id]
-    @vote.work_id = params[:work][:id]
+    @vote.user = params[:user_id]
+    @vote.work = params[:work_id]
     if @vote.save
       redirect_to :root
-    else
-      render :new
-    end
-
-  end
-
-  def show
-    @vote = Vote.find(params[:id])
-  end
-
-  def edit
-    @vote = Vote.find(params[:id])
-  end
-
-  def update
-    @vote = Vote.find(params[:id])
-    if @vote.update(vote_params)
-      redirect_to :root
-    else
-      render :edit
     end
   end
 
-  def destroy
-    @vote = Vote.find(params[:id]).destroy
-  end
 
   private
 
-  def vote_params
-    params.require(:votes).permit(:title, :type, :description, :published)
-  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resource :works
+  root to: 'works#index'
+  resources :works
+  resources :votes, only: [:show, :new]
+  resources :users, only: [:index, :show, :new]
+
+  post '/works/:work_id/votes', to: 'trips#create', as: 'new_trip'
+
+
 end
