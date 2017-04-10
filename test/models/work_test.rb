a87af7396639dd0349df211629da3eec126b00b3
Ann Dai <gofarann@gmail.com>
2017-04-10 16:30:13 -0700

    added tests for works creation

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -8,12 +8,18 @@ describe Work do
   end
 
   it "creates a new work" do
+    work = works(:work)
+    work.valid?.must_equal true
   end
 
   it "requires a category to create a new work" do
+    work = works(:work_no_category)
+    work.valid?.must_equal false
   end
 
   it "requires a title to create a new work" do
+    work = works(:work_no_title)
+    work.valid?.must_equal false
   end
 
 end
