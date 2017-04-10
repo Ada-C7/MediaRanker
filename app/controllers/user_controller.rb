26a62e12912d376843601a7c984f5a880afef0e6
Ting Wong <hellotingwong@gmail.com>
2017-04-10 16:10:15 -0700

    first draft of UserController

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,44 @@
 class UserController < ApplicationController
+    def index
+      @user = User.all
+    end
+
+    def new
+      @user = User.new(params[:id])
+    end
+
+    def create
+      @user = User.create(params[:id])
+    end
+
+    def edit
+      @user = User.find(params[:id])
+    end
+
+    def update
+      user = User.find(params[:id])
+      user.update_attributes(user_params)
+      user.save
+
+      redirect_to users_path
+    end
+
+    def delete
+      @user = User.find(params[:id])
+    end
+
+    def destroy
+      user = User.find(params[:id])
+      user.destroy
+
+      redirect_to users_path
+    end
+
+    private
+
+    def user_params
+      return params.require(:user).permit(:username)
+    end
+
+  end
 end
