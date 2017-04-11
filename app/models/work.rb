3a34a85c55eebb71367e190c51f4b8ff25db3562
Rana Sulaiman <rs_sulaiman@hotmail.com>
2017-04-11 16:58:15 -0700

    Created model for user and vote

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,9 +1,15 @@
 class Work < ApplicationRecord
 
-  def self.show_category
-     @books = Work.where(category: "book")
-    @albums = Work.where(category: "album")
-    @movies = Work.where(category: "movie")
+  # def self.show_category
+  #    @books = Work.where(category: "book")
+  #   @albums = Work.where(category: "album")
+  #   @movies = Work.where(category: "movie")
+  #
+  # end
+  validates :category, presence: true
+  validates :title, presence: true
+  validates :creator, presence: true
+  validates :publication_year, presence: true
+  validates :description, presence: true
 
-  end
 end
