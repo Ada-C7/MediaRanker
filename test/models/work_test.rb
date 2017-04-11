dd4a61cda0198f564c77b6deb1f3d2bb9b9b8741
Ashtn <ashtonehorton@gmail.com>
2017-04-11 14:17:40 -0700

    passed positive and negative work model validation for title

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -3,7 +3,21 @@ require "test_helper"
 describe Work do
   let(:work) { Work.new }
 
-  it "must be valid" do
-    value(work).must_be :valid?
+
+  # Write at least one test for each custom method on a model
+
+  # Write at least one test for each model relationship on a model
+
+  # Write at least two tests for each validation on a model
+  it "cannot create a work with a title" do
+    work.valid?.must_equal false
   end
+
+  it "can create a book" do
+    work.title = "The Great Great"
+    work.save.must_equal true
+  end
+  # Write at least two tests for each scope on a model (we’ll talk about scopes next week)
+
+
 end
