0e8567b0f627a0002b3569afe81b0dbe432b1228
natalia-ku <bilberryfm@mail.ru>
2017-04-10 17:26:48 -0700

    added index view and index method fir welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    @top_albums = Work.where(category: "album")
+    @top_books = Work.where(category: "movie")
+    @top_movies = Work.where(category: "book")
   end
 end
--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,6 +1,6 @@
 class WorksController < ApplicationController
   def index
-
+    @works = Work.all
   end
 
   def new
@@ -18,20 +18,23 @@ class WorksController < ApplicationController
   end
 
   def show
-    # @work = Work.find()
+     @work = Work.find(params[:id])
   end
 
   def edit
-
+    @work = Work.find(params[:id])
   end
 
   def update
-
-
+    work = Work.find(params[:id])
+    work.update_attributes(work_params)
+    work.save
+      # redirect_to work_path(work)
   end
 
   def destroy
-
+    work = Work.find(params[:id])
+    work.destroy
   end
 
 end
