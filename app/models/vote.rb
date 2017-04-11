fca35ee4489724a53bc6f0b9317c806a8704f396
Olivia <ssecretsharer@gmail.com>
2017-04-11 14:55:23 -0700

    added model has_many and belongs_to relationships

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,2 +1,7 @@
 class Vote < ApplicationRecord
+belongs_to :works
+belongs_to :users
+# validates :user_id, presence: true, numericality: { only_integer: true, greater_than: 0 }
+# validates :work_id, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
 end
