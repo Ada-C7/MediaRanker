f7272ed5c429339c18faae008337996a70cb5828
Bo Trethewey <botrethewey@gmail.com>
2017-04-11 00:07:03 -0700

    updated validations for the work category field

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,7 +1,7 @@
 class Work < ApplicationRecord
   has_many :votes
 
-  validates :category, presence: true, inclusion: { in: %w(Movie Book Album),
+  validates :category, presence: true, inclusion: { in: %w(movie book album),
     message: "%{value} is not a valid category" }, allow_nil: false
   validates :title, presence: true
 end
