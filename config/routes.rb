ac74cc89b91da1c15900fe726e75d9ee652567db
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-04-11 13:04:30 -0700

    created half of work show view, added links to titles on index and category views

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'works#index'
-  get '/works/movies', to: 'works#movies', as: 'movies'
-  get '/works/books', to: 'works#books', as: 'books'
-  get '/works/albums', to: 'works#albums', as: 'albums'
+  get 'works/movies', to: 'works#movies', as: 'movies'
+  get 'works/books', to: 'works#books', as: 'books'
+  get 'works/albums', to: 'works#albums', as: 'albums'
+  post '/works/:id/votes', to: 'votes#create', as: 'new_vote'
 
   resources :works
   resources :votes, only: [:show, :new]
   resources :users, only: [:index, :show, :new]
 
-  post '/works/:work_id/votes', to: 'trips#create', as: 'new_trip'
 
 
 end
