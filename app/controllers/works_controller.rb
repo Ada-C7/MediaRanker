8fd8ce2e73715d8fbf231aab7e17c0b01fe33699
Bo Trethewey <botrethewey@gmail.com>
2017-04-11 16:50:54 -0700

    Updated logic in Model and Controller for custom method in Work

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,58 @@
 class WorksController < ApplicationController
+  def index
+    @works = Work.all
+    @spotlight_work = Work.spotlight
+  end
+
+  def show
+    @result_work = Work.find(params[:id])
+  end
+
+  def create
+    @work = Work.new(work_params)
+  end
+
+  def edit
+    @work = Work.find(params[:id])
+  end
+
+  def update
+    @work = Work.find(params[:id])
+
+    if @work.update(work_params)
+
+      category = params[:category]
+      if category == "movie"
+        redirect_to movies_path
+      elsif category == "book"
+        redirect_to books_path
+      else # category == "album"
+        redirect_to albums_path
+      end
+
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    Trip.destroy(params[:id])
+
+    category = params[:category]
+    if category == "movie"
+      redirect_to movies_path
+    elsif category == "book"
+      redirect_to books_path
+    else # category == "album"
+      redirect_to albums_path
+    end
+
+  end
+
+
+  private
+
+  def work_params
+    params.require(:work).permit(:category, :title, :creator, :description, :publication_year)
+  end
 end
