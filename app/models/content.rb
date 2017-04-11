3dd949c7c7dbe32b3129cedf6bee6d340cf09327
Anacelia Gillette <acgillette426@gmail.com>
2017-04-11 12:42:28 -0700

    First model test made

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,2 +1,6 @@
 class Content < ApplicationRecord
+
+  validates :title, presence: true, uniqueness: true
+  validates :creator, presence: true
+  validates :publication_year, presence: true
 end
--- test/models/content_test.rb
+++ test/models/content_test.rb
@@ -3,7 +3,13 @@ require "test_helper"
 describe Content do
   let(:content) { Content.new }
 
-  it "must be valid" do
-    value(content).must_be :valid?
+  it "you can create new Content" do
+    content.category = "book"
+    content.title = "Test Book"
+    content.creator = "Me"
+    content.publication_year = "2009"
+    content.description = "This be test"
+
+    content.valid?.must_equal true
   end
 end
