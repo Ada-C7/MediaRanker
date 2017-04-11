adacb521175358b7fe620c69a5fd3beee43fdd2d
Anacelia Gillette <acgillette426@gmail.com>
2017-04-11 14:06:04 -0700

    Seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+content_info = File.read(Rails.root.join('db', 'media_seeds.csv'))
+content_info = CSV.parse(content_info, :headers => true)
+
+content_info.each do |row|
+  c = Content.new
+  c.category = row['category']
+  c.title = row['title']
+  c.creator = row['creator']
+  c.publication_year = row['publication_year']
+  c.description = row['description']
+  c.save
+end
+
+puts "There are #{Content.all.length} media pieces saved out of #{content_info.length}"
