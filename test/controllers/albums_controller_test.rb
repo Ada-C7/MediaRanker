fa69526f77953734722da01afd713181bbba5f78
Anacelia Gillette <acgillette426@gmail.com>
2017-04-11 14:01:30 -0700

    Added get index and show controller tests for Albums

--- test/controllers/albums_controller_test.rb
+++ test/controllers/albums_controller_test.rb
@@ -2,13 +2,13 @@ require "test_helper"
 
 describe AlbumsController do
   it "should get index" do
-    get albums_index_url
-    value(response).must_be :success?
+    get albums_path
+    must_respond_with :success
   end
 
-  it "should get show" do
-    get albums_show_url
-    value(response).must_be :success?
-  end
+  it "should show an individual album" do
+      get album_path(contents(:hacking).id)
+      must_respond_with :success
+    end
 
 end
