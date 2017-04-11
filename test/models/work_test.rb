39cd2927c3f17f28dede074a1d03ae5d215c3ce5
Ashtn <ashtonehorton@gmail.com>
2017-04-11 16:36:09 -0700

    created works controller

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -16,7 +16,26 @@ describe Work do
   it "can create a book" do
     work.title = "The Great Great"
     work.save.must_equal true
+end
+
+  it "Return data matches database data" do
+    one = works(:one)
+    one.title.must_equal 'MyString'
+    one.description.must_equal 'MyText'
+    one.year_published.must_equal 1
+    one.category.must_equal 'MyString'
+    one.creator.must_equal 'MyString'
   end
+
+  it "Non required fields can be left blank" do
+    one = works(:two)
+    one.title.must_equal 'MyString'
+    one.description.must_be_nil
+    one.year_published.must_equal 1
+    one.category.must_equal 'MyString'
+    one.creator.must_be_nil
+  end
+
   # Write at least two tests for each scope on a model (we’ll talk about scopes next week)
 
 
