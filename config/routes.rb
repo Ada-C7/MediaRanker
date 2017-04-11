56ce871e853d43e150aa4a5be0b3f59f8385d08b
Kayla <kubicke.kayla@gmail.com>
2017-04-11 15:26:51 -0700

    Established works, albums, movies, books, and user routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  
+
+  resources :works, except: [:index, :new, :create]
+  resources :albums, only: [:index, :new, :create]
+  resources :movies, only: [:index, :new, :create]
+  resources :books, only: [:index, :new, :create]
+  resources :users, only:  [:index, :show]
+
+  #NEED LOGIN ROUTE W/ POST
 end
