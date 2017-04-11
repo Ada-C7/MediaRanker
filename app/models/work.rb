ef2678e6ba884476694cf00ec7ad9279d85d7828
Elizabeth Deutsch <elizabeth.deutsch@att.net>
2017-04-11 08:57:33 -0700

    first tests and validations for work model

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,3 +1,11 @@
 class Work < ApplicationRecord
   has_many :votes
+  validates :category, presence: true
+  validates :title, presence: true
+  validates :creator, presence: true
+  validates :publication_year, presence: true
+  # validates :description, presence: true
+
+  # validates :name, presence: true, length: { minimum: 2 }
+  # validates :phone_num, presence: true
 end
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,9 +1,103 @@
 require "test_helper"
 
-describe Work do
-  let(:work) { Work.new }
 
-  it "must be valid" do
-    value(work).must_be :valid?
+describe Work do
+  describe "validations" do #success case
+    it "Can be created with all attributes" do
+      Work.create!(title: 'test title', creator: "ada", category: "album", publication_year: 1992, description: "great story!")
+      # result = work.valid?
+      # result.must_equal true
+    end
+
+    it "Requires a title" do #failure case test
+      work = Work.new
+      result = work.valid?
+      result.must_equal false
+
+      #Check that the validation we're testing
+      # did actually failure
+      work.errors.messages.must_include :title
+    end
+
+    it "Requires a category" do #failure case test
+      work = Work.new
+      result = work.valid?
+      result.must_equal false
+
+      #Check that the validation we're testing
+      # did actually failure
+      work.errors.messages.must_include :category
+    end
+
+    it "Requires a creator" do #failure case test
+      work = Work.new
+      result = work.valid?
+      result.must_equal false
+
+      #Check that the validation we're testing
+      # did actually failure
+      work.errors.messages.must_include :creator
+    end
+
+    it "Requires a publication_year" do #failure case test
+      work = Work.new
+      result = work.valid?
+      result.must_equal false
+
+      #Check that the validation we're testing
+      # did actually failure
+      work.errors.messages.must_include :publication_year
+    end
+
+    # it "Requires a description" do #failure case test
+    #   work = Work.new
+    #   result = work.valid?
+    #   result.must_equal false
+    #
+    #   #Check that the validation we're testing
+    #   # did actually failure
+    #   work.errors.messages.must_include :description
+    # end
+
+#     it "Must have a unique title" do
+#       a = Author.create!(name: "test author")
+#       # Add a book to the B to be a duplicate with
+#       book1 = Book.create!(title: 'duplicate title', author: a)
+#
+#       #Create a duplicate book
+#       book2 = Book.new(title: 'duplicate title', author: a)
+#
+#       result  = book2.valid?
+#       result.must_equal false
+#
+#       book2.errors.messages.must_include :title
+#     end
+#   end
+#
+#   describe Book do
+#     describe 'relations' do
+#       it 'can set the author through "author"' do
+#         # Create two models
+#         book = Book.new(title: "test book")
+#
+#         # Make the models relate to one another
+#         book.author = authors(:metz)
+#
+#         # author_id should have changed accordingly
+#         book.author_id.must_equal authors(:metz).id
+#       end
+#
+#       it 'can set the author through "author_id"' do
+#         # Create two models
+#         author = Author.create!(name: "test author")
+#         book = Book.new(title: "test book")
+#
+#         # Make the models relate to one another
+#         book.author_id = authors(:metz).id
+#
+#         # author should have changed accordingly
+#         book.author.must_equal authors(:metz)  #"authors" calls out the file authors.yml
+#       end
+#     end
   end
 end
