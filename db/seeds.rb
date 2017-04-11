c52207675f2f22aa4d4de9a38741c3252e108463
janicewilson <janice.wilson@mac.com>
2017-04-11 14:42:49 -0700

    Seeded.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,64 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+submissions = [
+     {
+          category: "Movie",
+          author: "George Lucas",
+          title: "Empire Strikes Back"
+     },
+     {
+           category: "Album",
+           author: "Prince",
+           title: "Purple Rain"
+     },
+     {
+          category: "Book",
+          author: "Conn Iggulden",
+          title: "Lords of the Bow"
+     }
+]
+
+submissions.each do | submission |
+  Submission.create(submission)
+end
+
+users = [
+     {
+          username: "Princess",
+          status: true
+     },
+     {
+           username: "Darth",
+           status: false
+     },
+     {
+          username: "Han",
+          status: true
+     }
+]
+
+users.each do | user |
+  User.create(user)
+end
+
+sf = Submission.first
+uf = User.first
+sl = Submission.last
+ul = User.last
+
+votes = [
+     {
+          submission: sf,
+          user: uf
+     },
+     {
+          submission: sl,
+          user: ul
+     },
+]
+
+votes.each do | vote |
+  Vote.create(vote)
+end
