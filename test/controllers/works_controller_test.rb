7b0b79ec480aedb4a5812f34aa972616f8e61791
Regan Huff <regan.huff@gmail.com>
2017-04-11 16:47:54 -0700

    Add test for index controller actionin Works

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -2,8 +2,8 @@ require "test_helper"
 
 describe WorksController do
   it "should get index" do
-    get works_index_url
-    value(response).must_be :success?
+    get works_path
+    must_respond_with :success
   end
 
 end
