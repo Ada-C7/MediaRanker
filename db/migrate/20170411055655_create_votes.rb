abe851eb7dd713ab997bd63126a3bf2bca38dbc2
Kaitlin Ramirez <rmrz.kt@gmail.com>
2017-04-10 23:23:59 -0700

    added column information to tables for migrations

--- db/migrate/20170411055655_create_votes.rb
+++ db/migrate/20170411055655_create_votes.rb
@@ -1,6 +1,9 @@
 class CreateVotes < ActiveRecord::Migration[5.0]
   def change
     create_table :votes do |t|
+      
+      t.belongs_to :work
+      t.belongs_to :user
 
       t.timestamps
     end
