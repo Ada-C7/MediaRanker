da48f1996db63b66e1aca63fc667b463274edaef
laura <lmelgarejos@unal.edu.co>
2017-04-11 14:20:52 -0700

    Views for each media created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+csv_text = File.read('db/media_seeds.csv')
+
+csv = CSV.parse(csv_text, :headers => true)
+csv.each do |row|
+  w = Work.new
+  w.category = row['category']
+  w.title = row['title']
+  w.created_by = row["creator"]
+  w.published_year = row["publication_year"]
+  w.description = row["description"]
+  w.save
+end
