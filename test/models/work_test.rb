afc23f26280f15ef4cbe8a2e7afdd7bf10fc460f
Regan Huff <regan.huff@gmail.com>
2017-04-11 16:20:39 -0700

    Start index page and tweak work model tests

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -3,12 +3,12 @@ require "test_helper"
 describe Work do
   let(:work) { Work.new }
 
-  it "cannot create a work without a title" do
+  it "can't create a work without a title" do
     work.valid?.must_equal false
     work.errors.messages.must_include :title
   end
 
-  it "cannot create a work if the title is not unique" do
+  it "can't create a work if title is duplicate for that category" do
     work = works(:elemental)
     work.save.must_equal true
 
@@ -18,6 +18,18 @@ describe Work do
     work2.save.must_equal false
     work2.valid?
     work2.errors.messages.must_include :title
+  end
+
+  it "can create work with duplicate title but different category" do
+    work = works(:elemental)
+    work.save.must_equal true
+
+    work2 = Work.new
+    work2.category = "album"
+    work2.title = work.title
+    work2.save.must_equal true
+    work2.valid?
+    work2.errors.messages[:title].must_equal []
   end
 
   it "can create a work if a unique title is provided" do
