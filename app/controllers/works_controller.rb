1cd6923e690ccdaf0d597f913631746618ce8bb5
Alix Hamilton <alix.hamilton@gmail.com>
2017-04-11 11:49:13 -0700

    Creates empty Work Controller methods and strong params

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,29 @@
 class WorksController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def work_params
+    params.require(:work).permit(:category, :title, :description, :creator, :publication_year)
+  end
+
 end
