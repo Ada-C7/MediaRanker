d3aa1b0410fa0ffcb832fb75eaa70fe02cae1d70
Kelly Souza <kellysouza99@gmail.com>
2017-04-11 14:35:15 -0700

    Adds show_category method in works controller

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,4 +1,22 @@
 class WorksController < ApplicationController
   def index
+    @works = Work.all
+    # .sort {|d1, d2| d1.id <=> d2.id}
+
+  end
+
+  def show
+    @work=Work.find(params[:id])
+  end
+
+  def show_category
+    @categorized_works = []
+    @work = Work.all
+    @work.each do |work|
+      if work.category == params[:category].singularize
+        @categorized_works << work
+      end
+    end
+    return @categorized_works
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+
+# category,title,creator,publication_year,description
+
+CSV.read("#{ Rails.root }/db/media_seeds.csv", {:headers => true}).each do |work|
+  args = {
+          :category => work[0],
+          :title => work[1],
+          :created_by => work[2],
+          :year_published => work[3],
+          :description => work[4],
+        }
+  Work.create(args)
+end
