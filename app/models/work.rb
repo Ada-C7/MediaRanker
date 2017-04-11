a6b2427a53d693261ee7ac543c86b6dea66d060b
Regan Huff <regan.huff@gmail.com>
2017-04-11 14:10:08 -0700

    Add tests for presence and uniqueness of title within category

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,3 +1,6 @@
 class Work < ApplicationRecord
-  validates :title, presence: true, uniqueness: true
+  validates :title, presence: true
+
+  #title must be unique within each category
+  validates :title, uniqueness: { scope: :category, message: "has already been taken"}
 end
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -3,7 +3,28 @@ require "test_helper"
 describe Work do
   let(:work) { Work.new }
 
-  it "must be valid" do
-    value(work).must_be :valid?
+  it "cannot create a work without a title" do
+    work.valid?.must_equal false
+    work.errors.messages.must_include :title
   end
+
+  it "cannot create a work if the title is not unique" do
+    work = works(:elemental)
+    work.save.must_equal true
+
+    work2 = Work.new
+    work2.category = "book"
+    work2.title = work.title
+    work2.save.must_equal false
+    work2.valid?
+    work2.errors.messages.must_include :title
+  end
+
+  it "can create a work if a unique title is provided" do
+    work.title = "Test"
+    work.valid?
+    work.errors.messages[:title].must_equal []
+  end
+
+
 end
