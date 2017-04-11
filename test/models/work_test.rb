e96870efdf0b4b5911d97361234129203ca848f5
Bo Trethewey <botrethewey@gmail.com>
2017-04-10 22:38:28 -0700

    added validations to work model(WIP)

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -3,7 +3,14 @@ require "test_helper"
 describe Work do
   let(:work) { Work.new }
 
-  it "must be valid" do
-    value(work).must_be :valid?
+  describe 'validations' do
+    it "is invalid without a title" do
+      work.valid?.must_equal false
+    end
+
+    it "is invlaid without a valid category" do
+      work.valid?.must_equal false
+    end
   end
+
 end
