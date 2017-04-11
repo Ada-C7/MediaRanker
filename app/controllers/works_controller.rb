fab226c807f08d863752d04cdaa26f8efdb07bc3
Chantal Rollison <rollison.chantal@gmail.com>
2017-04-10 19:39:39 -0700

    added controller actions for works

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,16 +1,34 @@
 class WorksController < ApplicationController
+
   def index
+    @works = Work.all
+  end
+
+  def show
+    @result_work = Work.find(params[:id])
   end
 
   def new
+    @work = Work.new
   end
 
   def update
   end
 
-  def delete
+  def destroy
+    Work.destroy(params[:id])
+
+    redirect_to works_path
   end
 
-  def show
+  def create
+    @work = Work.create work_params
+
+    if @work.id != nil
+      redirect_to works_path
+    else
+      render "new"
+    end
+
   end
 end
