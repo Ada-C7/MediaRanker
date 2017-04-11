e0ed99ee2032ed2db0bf42e8a8c78293acbaab06
Tamiko Terada <tamikodevo@gmail.com>
2017-04-10 22:17:31 -0700

    test(model)-work must have a title

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,9 +1,17 @@
 require "test_helper"
 
 describe Work do
-  let(:work) { Work.new }
+  # let(:work) { Work.new }
+  #
+  # it "must be valid" do
+  #   value(work).must_be :valid?
+  # end
 
-  it "must be valid" do
-    value(work).must_be :valid?
+  it 'must be created with a title' do
+    work = Work.new
+    result = work.valid?
+    result.must_equal false
+
+    work.errors.messages.must_include :title
   end
 end
