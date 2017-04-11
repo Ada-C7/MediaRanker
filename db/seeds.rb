3db87f7c4c5852ca6319de2c4f728140fdce6483
Haby Randall <sow.haby@gmail.com>
2017-04-11 13:22:51 -0700

    Editing seed file to bring in CSV

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+CSV.open('db/media_seeds.csv', 'r', :headers => true).each do |line|
+  category = line["category"].to_s
+  title = line["title"].to_s
+  creator =  line["creator"].to_s
+  publish_year = line["publication_year"].to_s
+  description = line["description"].to_s
+  # driver_array << {name: name, vin: vin, availability: true}
+  Work.create({category: category, title: title, creator: creator, publish_year: publish_year, description: description})
+end
