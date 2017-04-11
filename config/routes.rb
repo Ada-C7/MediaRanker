ee510ad9f87634f1e0628dedd6c71699fe2c4350
Jackie Watanabe <sushiigirl@gmail.com>
2017-04-11 14:05:25 -0700

    made work titles clickable links that go to broken pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get '/works', to: 'works#index'
 
+  get '/works/:id', to: 'works#show', as: 'work'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
