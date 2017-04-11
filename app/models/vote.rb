4dc5daa8589e2aa2ca7d6c2c4c1c53a9dcf31026
Lynn Trickey <lynn.trickey@gmail.com>
2017-04-11 16:59:30 -0700

    Created and passed first test for Vote model.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,3 +1,5 @@
 class Vote < ApplicationRecord
   belongs_to :work
+
+  #something to validate that each person can only vote for one work
 end
--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -3,7 +3,9 @@ require "test_helper"
 describe Vote do
   let(:vote) { Vote.new }
 
-  it "must be valid" do
-    value(vote).must_be :valid?
+  it "Cannot create a vote without a work" do
+    vote.valid?.must_equal false
+    vote.errors.must_include :work
   end
+
 end
