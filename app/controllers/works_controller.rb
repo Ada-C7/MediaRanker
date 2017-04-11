32bae4f1e39d201a3f23d652d11331a8165daa9d
Brenna <bcmdarroch@gmail.com>
2017-04-10 17:26:23 -0700

    defined WorksController methods, made views, wrote header html in application

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,49 @@
 class WorksController < ApplicationController
+
+  def index
+    @works = Work.all
+  end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work = Work.create(work_params)
+    if @work.save
+      redirect_to :root
+    else
+      render :new
+    end
+  end
+
+  def show
+    @work = Work.find(params[:id])
+  end
+
+  def edit
+    @work = Work.find(params[:id])
+  end
+
+  def update
+    @work = Work.find(params[:id])
+    @work.update(work_params)
+    if @work.save
+      redirect_to :root
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Work.destry(params[:id])
+    redirect_to :root
+  end
+
+  private
+
+  def work_params
+    params.require(:work).permit(:name, :created_by, :pub_year, :desc)
+  end
+
 end
