ac0936cc7ee9d1a145243fe92ff30b53ae86be03
add2point71dots <alcland@gmail.com>
2017-04-11 13:15:16 -0700

    got the database seed working

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+works = CSV.read("db/media_seeds.csv")[1..-1].map do |line|
+  {
+    category: line[0],
+    title: line[1],
+    creator: line[3],
+    year_published: line[4],
+    description: line[5]
+  }
+end
+
+work_successes = 0
+works.each do |work|
+  work = Work.create(work)
+  if work.id
+    work_successes += 1
+  else
+    puts "FAILED ON #{work[:title]}"
+  end
+end
+
+puts "Created #{work_successes} out of #{works.length} works."
