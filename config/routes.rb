1f4262ec93cdf1ad96877524baacced6526e59e3
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-04-11 15:36:22 -0700

    cleaned up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'homepage#index', as:'homepage'
   # need to change this - dont need all the restful routes
-  resources :works
+  resources :works, except: [:index, :new, :edit]
 
 
   get 'movies', to: 'works#movies', as: 'movies'
