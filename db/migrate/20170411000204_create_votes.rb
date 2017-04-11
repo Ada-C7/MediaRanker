5dd4d1ac5150c319e9de34ed60340ad86217d942
Andrea Valliere <amvalliere@gmail.com>
2017-04-11 16:11:50 -0700

    Added relationships and migrated db

--- db/migrate/20170411000204_create_votes.rb
+++ db/migrate/20170411000204_create_votes.rb
@@ -1,8 +1,11 @@
 class CreateVotes < ActiveRecord::Migration[5.0]
   def change
     create_table :votes do |t|
+      t.belongs_to :user
+      t.belongs_to :work
 
       t.timestamps
+
     end
   end
 end
