67e53f2e1f02983742c841fc868979166d5acbe6
Jackie Watanabe <sushiigirl@gmail.com>
2017-04-11 14:35:12 -0700

    created one model test

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,5 @@
 class Work < ApplicationRecord
+  validates :title, presence: true, uniqueness: true
+  # 
+  # validates :isbn, uniqueness: true
 end
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -3,7 +3,29 @@ require "test_helper"
 describe Work do
   let(:work) { Work.new }
 
-  it "must be valid" do
-    value(work).must_be :valid?
+  it "Works must have titles" do
+    work.valid?.must_equal false
+    #hash of all message
+    work.errors.messages.must_include :title
   end
+
+  it "If a title is given the work is valid" do
+  book.title = "The Wizard of Oz"
+  book.valid? #alternatively book.save.... book won't save until save.. or if valid is run, it will check the database for duplicates...but valid is more explicit about what we're trying to do here...which is test
+
+# save will always run the validations
+  book.errors.messages[:title].must_equal []
+end
+
+#postive test
+it "You can create a book" do
+  book.title = "The Wizard of Oz"
+  book.isbn = "1111"
+
+  author = Author.new name: "Kari"
+  book.author = author
+
+  book.valid?.must_equal true
+end
+
 end
