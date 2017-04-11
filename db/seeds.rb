c8bf343d3307e83f2f40e8007eb145feced910ad
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-04-11 12:46:10 -0700

    Created seeds from media_seeds.csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+
+CSV.foreach("db/media_seeds.csv", :headers => true) do |line|
+
+  p = Work.new
+  p.mediatype = line[1].to_s
+  p.title = line[2].to_s
+  p.creator = line[3].to_s
+  p.year = line[4].to_i
+  p.description = line[5].to_s
+  p.save
+
+  puts "#{p.title} saved"
+end
