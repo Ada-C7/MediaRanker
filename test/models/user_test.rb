6db37c3207b0ba50e76ad05bd850c23ce6e22806
Hyunji Kim <ricecakemonster@gmail.com>
2017-04-11 14:53:12 -0700

    Added user tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,23 @@
 require "test_helper"
 
 describe User do
-  let(:user) { User.new }
+#   let(:user) { User.new }
+  it "Can be created with all attributes" do
+   user = User.new(username: "whatever")
+   result = user.valid?
+   result.must_equal true
+  end
 
-  it "must be valid" do
-    value(user).must_be :valid?
+  it "is invalid without a username" do
+    user = User.new
+    result = user.valid?
+    result.must_equal false
+  end
+
+  it "Must have a unique username" do
+    user = User.create!(username: "whatever")
+    user1 = User.new(username: "whatever")
+    result = user1.valid?
+    result.must_equal false
   end
 end
