464d06eaca7eb67721e1a474cc39db745f58b05b
Regan Huff <regan.huff@gmail.com>
2017-04-11 16:28:31 -0700

    Tweak work model tests

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,18 +1,15 @@
 require "test_helper"
 
 describe Work do
-  let(:work) { Work.new }
+  let(:work) { works(:elemental) }
+  let(:work2) { Work.new }
 
   it "can't create a work without a title" do
-    work.valid?.must_equal false
-    work.errors.messages.must_include :title
+    work2.valid?.must_equal false
+    work2.errors.messages.must_include :title
   end
 
   it "can't create a work if title is duplicate for that category" do
-    work = works(:elemental)
-    work.save.must_equal true
-
-    work2 = Work.new
     work2.category = "book"
     work2.title = work.title
     work2.save.must_equal false
@@ -21,10 +18,6 @@ describe Work do
   end
 
   it "can create work with duplicate title but different category" do
-    work = works(:elemental)
-    work.save.must_equal true
-
-    work2 = Work.new
     work2.category = "album"
     work2.title = work.title
     work2.save.must_equal true
@@ -32,11 +25,11 @@ describe Work do
     work2.errors.messages[:title].must_equal []
   end
 
-  it "can create a work if a unique title is provided" do
-    work.title = "Test"
-    work.valid?
-    work.errors.messages[:title].must_equal []
+  it "can create a work in same category if unique title is provided" do
+    work2.category = "book"
+    work2.title = "A totally other thing"
+    work2.save.must_equal true
+    work2.valid?
+    work2.errors.messages[:title].must_equal []
   end
-
-
 end
