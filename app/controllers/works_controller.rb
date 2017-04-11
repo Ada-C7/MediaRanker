66a7e574a384dc5581b70b9e0abec6b1b3e22e4b
Kaitlin Ramirez <rmrz.kt@gmail.com>
2017-04-11 15:35:43 -0700

    skeleton mainpage link works. album route made. need partial

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,23 @@
 class WorksController < ApplicationController
+  # View media by category
+  def index
+      @works = Work.all
+  end
+
+  def movie_index
+    # @movies = Work[category: movie]
+  end
+
+  def book_index
+    # @books = Work[category: book]
+  end
+
+  def album_index
+    @albums = Work.where(category: "album")
+  end
+
+  def show
+    @work = Work.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'mainpages#index', as: 'mainpages'
+
+  get 'albums', to:'works#album_index', as: 'albums'
 end
