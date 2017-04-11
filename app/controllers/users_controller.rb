8cd90b717786134cd6286fa564bc67cb5f63b671
Theresa Manney <theresa.m.17@gmail.com>
2017-04-11 14:25:05 -0700

    Reset my db to fix migration problem, creted user table, created routes and forms for user table...WIP

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
+  def show
+    @result_user = User.find_by_id(params[:id])
+  end
   def new
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
-  get 'works/index'
+  get '/users', to: 'users#index'
+  get '/users/:id', to: 'users#show', as: 'user'
 
-  get 'users/index'
+  get 'works/index'
 
   get 'books/index'
 
