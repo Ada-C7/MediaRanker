64fbae1e3c5c72e000d508239f767eb2eef2be4d
Chantal Rollison <rollison.chantal@gmail.com>
2017-04-11 16:31:09 -0700

    added index pages for all

--- app/controllers/media_ranker_controller.rb
+++ app/controllers/media_ranker_controller.rb
@@ -1,6 +1,8 @@
 class MediaRankerController < ApplicationController
 
   def index
-    @works = Work.all
+    @albums = Work.where(category: "album")
+    @books = Work.where(category: "book")
+    # @movies = Work.where(category: "movie")
   end
 end
