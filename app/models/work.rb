0a127524fc070bd01b2fa54b60511dc2eac25f60
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-04-10 18:23:57 -0700

    tests for work all passing -- all validations present at this point

--- app/models/work.rb
+++ app/models/work.rb
@@ -2,5 +2,11 @@ class Work < ApplicationRecord
 
   has_many :votes
   belongs_to :user
-  
+
+  validates :title, presence: true
+  validates :published, presence: true
+  validates :description, presence: true
+  validates :category, presence: true
+  validates_inclusion_of :category, :in => ["book", "movie", "album"], :allow_nil => true
+
 end
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,20 +1,57 @@
 require "test_helper"
 
 describe Work do
-  let(:work) { Work.new }
+  let(:work) { Work.new(user: users(:aurora)) }
 
   # must have a title
+  it "cannot create a work without a title" do
+    work.valid?.must_equal false
+  end
 
   # must have a published
+  it "cannot create a work without a published" do
+    work.title = "bolivia"
+    work.valid?.must_equal false
+  end
 
   # must have a description
+  it "cannot create a work without a description" do
+    work.title = "bolivia"
+    work.published = "1985"
+    work.valid?.must_equal false
+  end
 
   # must have a category
+  it "cannot create a work without a category" do
+    work.title = "bolivia"
+    work.published = "1985"
+    work.description = "A lot of stuff about south america."
+    work.valid?.must_equal false
+  end
 
   # category must be "movie", "book", or "album"
-
-  # must have a user_id
+  it "category must be movie, book, or album" do
+    work.title = "bolivia"
+    work.published = "1985"
+    work.description = "A lot of stuff about south america."
+    work.category = "movie"
+    work.valid?.must_equal true
+    work.category = "book"
+    work.valid?.must_equal true
+    work.category = "album"
+    work.valid?.must_equal true
+    work.category = "poo"
+    work.valid?.must_equal false
+    work.category = 0
+    work.valid?.must_equal false
+    work.category = nil
+    work.valid?.must_equal false
+  end
 
   # must be able to create new work
+  it "must be able to create new work" do
+    work = Work.new(title: "big stuff", published: 1986, description: "Some stuff here.", user: users(:felix), category: "movie")
+    work.valid?.must_equal true
+  end
 
 end
