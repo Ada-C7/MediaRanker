7794311df12b3cea932947d0aa4245705decde82
sai samant <saisamant@gmail.com>
2017-04-10 20:27:05 -0700

    added CRUD controller actions for WorksController

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -7,17 +7,42 @@ class WorksController < ApplicationController
 
 
   def show
+    @selected_work = Work.find params[:id]
   end
 
   def create
+    @work = Work.create work_params
+
+    unless @work.id ==  nil
+      redirect_to workers_path
+    else
+      render "new"
+    end
   end
 
   def edit
+    @work = Work.find params[:id]
   end
 
   def update
+    @work = Work.find params[:id]
+
+    if @work.save
+      redirect_to workers_path
+    else
+      render "edit"
+    end
   end
 
   def destroy
+    Work.destroy params[:id]
+    redirect_to workers_path
   end
+
+
+  private
+  def work_params
+    params.require(:work).permit(:title, :creator, :category, :year, :description)
+  end
+
 end
