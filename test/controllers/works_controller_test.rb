30594a57fc9aeb87ee2f9707b96bbc1a96eafda0
natalia-ku <bilberryfm@mail.ru>
2017-04-11 10:52:45 -0700

    added tests for works controller

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 describe WorksController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "index" do
+    it "responds successfully" do
+      Work.count.must_be :>, 0
+      get works_path
+      must_respond_with :success # success -  http status code
+    end
+    it "still responds successfully when there are no works " do
+      Work.destroy_all
+      get works_path
+      must_respond_with :success
+    end
+  end
+  describe "new" do
+
+  end
+
 end
