d344a326291eb39348d73da7577dfe6e72236911
Lynn Trickey <lynn.trickey@gmail.com>
2017-04-11 13:15:31 -0700

    Added negative test for edit

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -6,11 +6,13 @@ describe WorksController do
     must_respond_with :success
   end
 
+  #pos show test
   it "should get show page" do
     get work_path(works(:work_one).id)
     must_respond_with :success
   end
 
+  #neg show test
   it "should show a 404 if work not found" do
     get work_path(1)
     must_respond_with :missing
@@ -22,9 +24,15 @@ describe WorksController do
   end
 
 
-
+  #pos edit test
   it "should get form to edit work" do
     get edit_work_path((works(:work_one).id))
     must_respond_with :success
+  end
+
+  #neg edit test
+  it "should show a 404 if work not found to edit" do
+    get edit_work_path(1)
+    must_respond_with :missing
   end
 end
