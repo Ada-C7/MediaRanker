da710c3e49048dd77a436cab040bfed4430300b7
add2point71dots <alcland@gmail.com>
2017-04-11 17:07:57 -0700

    made show method, view, and tests

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -4,4 +4,9 @@ class WorksController < ApplicationController
     @albums = Work.albums
     @books = Work.books
   end
+
+  def show
+    @work = Work.find_by_id(params[:id])
+    render_404 if !@work
+  end
 end
--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -5,4 +5,24 @@ describe WorksController do
     get works_path
     must_respond_with :success
   end
+
+  it "should show one book" do
+    get work_path(works(:bone_people))
+    must_respond_with :success
+  end
+
+  it "should show one movie" do
+    get work_path(works(:graduate))
+    must_respond_with :success
+  end
+
+  it "should show one album" do
+    get work_path(works(:libertines).id)
+    must_respond_with :success
+  end
+
+  it "should show a 404 when media not found" do
+    get work_path(1)
+    must_respond_with :missing
+  end
 end
