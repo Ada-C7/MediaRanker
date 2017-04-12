1ed93d709eccb613720d5c7fb0b464a9d1e027cf
Lynn Trickey <lynn.trickey@gmail.com>
2017-04-12 15:06:07 -0700

    Added login/logout controller actions for sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,21 @@ class SessionsController < ApplicationController
   def login_form; end
 
   def login
-    # need to create user model first!
-    # user = User.find_by_name
+    user = User.find_by_name(params[:name])
+
+    if user
+      flash[:success] = "Successfully logged in as #{ user.name }"
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      #should create a new user (call user method??  REDIRECT?) if none found
+    end
+  end
+
+  def logout
+    session[:user_id] = nil
+    flash[:success] = "Successfully logged out"
+    redirect_to root_path
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'works#index'
+
   resources :works
 
   get '/books', to: 'works#index_books', as: 'books'
@@ -8,4 +10,5 @@ Rails.application.routes.draw do
 
   get '/login', to: 'sessions#login_form'
   post '/login', to: 'sessions#login'
+  delete '/logout', to: 'sessions#logout'
 end
