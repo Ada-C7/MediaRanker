1f182e51a37b7f7c35280b3393d085cf0cbf3544
EricaJCase <ericajcase@gmail.com>
2017-04-11 22:50:16 -0700

    vote added

--- db/migrate/20170412051310_create_votes.rb
+++ db/migrate/20170412051310_create_votes.rb
@@ -1,7 +1,8 @@
 class CreateVotes < ActiveRecord::Migration[5.0]
   def change
     create_table :votes do |t|
-
+      t.belongs_to :user
+      t.belongs_to :work
       t.timestamps
     end
   end
