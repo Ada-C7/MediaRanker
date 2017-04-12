7a5296d89525828614ae953ff064c472e1238085
Cara Comfort <cara.comfort10@gmail.com>
2017-04-12 15:13:05 -0700

    Create work create route and controller
    Modify new work view so that request is sent to works/create instead of
    albums/create, movies/create, and books/create. Add a hidden view to
    submit work category.

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -8,4 +8,21 @@ class WorksController < ApplicationController
     @category = params[:works]
     @work = Work.new
   end
+
+  def create
+    @work = Work.new(work_params)
+    category = params[:work][:category].to_s.pluralize
+
+    if @work.save
+      redirect_to works_path(category)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def work_params
+    params.require(:work).permit(:title, :creator, :publication_year, :description, :category)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   has_many :votes
+  # has_many :works, through: :votes #how to test???????
 end
