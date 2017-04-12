350ab8f9d2eb3396cc9c241f86c173034765169d
Olivia <ssecretsharer@gmail.com>
2017-04-12 15:29:59 -0700

    Added back in the session controller code that I lost

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,30 @@
 class SessionsController < ApplicationController
+
+  def login_form; end
+
+
+  def login
+    #raise
+    author = Author.find_by_name(params[:name])
+
+    if author
+      #logic for found
+      session[:author_id] = author.id
+      flash[:success] = "Hello, #{ author.name }"
+      redirect_to root_path
+    else
+      #did not find
+      flash.now[:error] = "Author not found."
+      render :login_form #in this case, this is the same as render "new"
+    end
+  end
+
+  def logout
+    #session[:author_id] = nil
+    session.delete[:author_id]
+    flash[:success] = "You are successfully logged out."
+    redirect_to root_path
+  end
+
+
 end
