636d408888f6aee67984ee8c16298a2edbf16c9b
Haby Randall <sow.haby@gmail.com>
2017-04-11 23:42:33 -0700

    Adding fixtures and validations to begin testing model

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,6 @@
 class Work < ApplicationRecord
+  validates :category, presence: true, inclusion: { in: %w(book movie album) }
+  validates :title, presence: true, uniqueness: true
+  validates :creator, presence: true
+  validates :publish_year, presence: true
 end
