89831b9d2c9ebe6dba1f6e350aacbd4e8f5e5436
Regan Huff <regan.huff@gmail.com>
2017-04-12 15:25:19 -0700

    Add login method and display for flash messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,20 @@ class SessionsController < ApplicationController
   end
 
   def login
-    
+    user = User.find_by_name(params[:user][:name])
+
+    if user
+      #found successfully
+      session[:user_id] = user.id
+      flash[:success] = "Successfully logged in as existing user #{user.name}"
+      
+      redirect_to root_path
+    else
+      #did not find; create new user with name from params
+      new_user = User.create(name: params[:user][:name])
+      flash[:success] = "Successfully created new user #{new_user.name} with ID #{new_user.id}"
+
+      redirect_to root_path
+    end
   end
 end
