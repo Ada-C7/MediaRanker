49575b018ab92758ef35d4cb3ea9449f4f318555
Regan Huff <regan.huff@gmail.com>
2017-04-12 06:32:59 -0700

    Add random work for spotlight on home page

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -3,10 +3,12 @@ class WorksController < ApplicationController
     @albums = Work.where(category: "album")
     @books = Work.where(category: "book")
     @movies = Work.where(category: "movie")
+
+    @spotlight = Work.all.sample
   end
 
   def show
     @work = Work.find_by_id(params[:id])
-  
+
   end
 end
