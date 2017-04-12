b4a18bedba466aeccf5b86a690294f4d3a9900e7
Ting Wong <hellotingwong@gmail.com>
2017-04-11 22:11:56 -0700

    first draft of vote model tests

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,7 +1,47 @@
 require "test_helper"
 
 describe Vote do
-  describe "relationships" do
+  describe "user relationships" do
+    it 'can set the user through "user"' do
+      user = User.create!(username: "Test User")
+      #include work if validations are needed?
+      # work = Work.create!(title: "Test", creator: "Test Creator", category: "movie")
+      vote = Vote.new
+      vote.user = user
 
+      vote.user_id.must_equal user.id
+    end
+
+    it 'can set the user through "user_id"' do
+      user = User.create!(username: "Test User")
+      #include work if validations are needed?
+      # work = Work.create!(title: "Test", creator: "Test Creator", category: "movie")
+      vote = Vote.new
+      vote.user_id = user.id
+      
+      vote.user.must_equal user
+    end
+  end
+
+  describe "work relationships" do
+    it 'can set the work through "work"' do
+      work = Work.create!(title: "Test", creator: "Test Creator", category: "movie")
+      #include user if validations are needed?
+      # user = User.create!(username: "Test User")
+      vote = Vote.new
+      vote.work = work
+
+      vote.work_id.must_equal work.id
+    end
+
+    it 'can set the work through "work_id"' do
+      work = Work.create!(title: "Test", creator: "Test Creator", category: "movie")
+      #include user if validations are needed?
+      # user = User.create!(username: "Test User")
+      vote = Vote.new
+      vote.work_id = work.id
+
+      vote.work.must_equal work
+    end
   end
 end
