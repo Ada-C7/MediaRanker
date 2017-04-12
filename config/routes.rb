58aa03c37e00b7d4a9b56c222201efda016d49eb
Alix Hamilton <alix.hamilton@gmail.com>
2017-04-11 18:37:35 -0700

    removes redundant routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
   root to: "works#index", category: "all"
-  
+
   resources :books, controller: "works", category: "book"
   resources :albums, controller: "works", category: "album"
   resources :movies, controller: "works", category: "movie"
-
-  %w(book album movie).each do |category|
-    get "/#{category.pluralize}", to: 'works#show_category', category: category
-  end
-
 end
