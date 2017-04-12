da953dfa1d4ab34a04b739f8479e1f4f90f193f4
jjousun <jjousun@gmail.com>
2017-04-11 17:04:39 -0700

    Added link of new on the Index page for each category

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "mediaranker#index"
 
+  get "/:category/new", to: "works#new", as: "new_category"
+  post "/:category", to: "works#create"
+
   get "/:category", to: "works#show_category", as: "category"
 
   resources :works
