b6e44ad15729f62344e589cd6cc22c20d9d690e0
sai samant <saisamant@gmail.com>
2017-04-11 22:35:04 -0700

    testing controller WIP

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -1,19 +1,31 @@
 require "test_helper"
 
 class WorksControllerTest < ActionDispatch::IntegrationTest
+
 describe WorksController do
   it "should get index" do
     get works_path
     must_respond_with :success
   end
 
-  it "should get show" do
-    get works_show_url
-    value(response).must_be :success?
+  it "should show a work with a valid id" do
+    get work_path(works(:amelie).id)
+    must_respond_with :success
+  end
+
+  it "should be missing if id is not valid" do
+    skip #need to fix the other piece for this
+    get work_path(2)
+    must_respond_with :missing
+  end
+
+  it "should get the new view" do
+    get new_work_path
+    must_respond_with :success
   end
 
   it "should get create" do
-    get works_create_url
+    get
     value(response).must_be :success?
   end
 
