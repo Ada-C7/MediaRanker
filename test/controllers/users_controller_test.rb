f50885269936441f1a7724bab0c429670e524fd3
Kayla <kubicke.kayla@gmail.com>
2017-04-12 07:34:39 -0700

    Added index method test.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,14 @@
 require "test_helper"
 
 describe UsersController do
-  it "Responds" do
+  it "Responds correctly" do
     User.count.must_be :>, 0
+    get users_path
+    must_respond_with :success
+  end
+
+  it "Responds without any users" do
+    User.destroy_all
     get users_path
     must_respond_with :success
   end
