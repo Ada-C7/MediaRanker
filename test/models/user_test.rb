62506eca77c686e5930bb9946cc0dd04ddcfc448
Lynn Trickey <lynn.trickey@gmail.com>
2017-04-12 14:42:32 -0700

    Finshed initial testing for user model:

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,25 @@ require "test_helper"
 describe User do
   let(:user) { User.new }
 
-  it "must be valid" do
-    value(user).must_be :valid?
+  it "cannot create a user without a name" do
+    user.valid?.must_equal false
+    user.errors.must_include :name
   end
+
+  it "can create a user" do
+    user.name = "lynn"
+    user.valid?.must_equal true
+  end
+
+  it "Name must be unique" do
+    user.name = "Lynn"
+    user.save
+
+    other_user = User.new
+    other_user.name = "Lynn"
+
+    other_user.save
+    other_user.errors.messages.must_include :name
+  end
+
 end
