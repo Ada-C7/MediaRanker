845bf995547644fff4e782d922dbd01d1c13cb48
Regan Huff <regan.huff@gmail.com>
2017-04-12 14:40:52 -0700

    Create Sessions controller and login routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   root "works#index"
 
+  resources :works
+
   get "/albums", to: "albums#index"
-
   get "/books", to: "books#index"
-
   get "/movies", to: "movies#index"
 
+  get "/login", to: "sessions#login_form"
+  post "/login", to: "sessions#login"
 
-  resources :works
+
 
 end
