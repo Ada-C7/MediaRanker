e46085c580a4591a6a048f89f5c9f6b7d762fde7
Jackie Watanabe <sushiigirl@gmail.com>
2017-04-12 00:19:18 -0700

    added link to add new work on album list, movie list, and book list views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def index
-    @works = Work.all
+  @works = Work.all
   @movies = Work.all.select {|work| work.category == "movie"}
   @books = Work.all.select {|work| work.category == "book"}
   @albums = Work.all.select {|work| work.category == "album"}
