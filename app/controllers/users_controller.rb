420098cc8980851bd75bc80bac42e488e4956eb1
Kayla <kubicke.kayla@gmail.com>
2017-04-11 20:07:30 -0700

    Moved album, movie, and book methods into works controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,41 +1,9 @@
 class UsersController < ApplicationController
+  
   def index
   end
 
   def show
-  end
-
-  # ALBUMS
-
-  def albums_index
-  end
-
-  def create_album
-  end
-
-  def new_album
-  end
-
-  # MOVIES
-
-  def movies_index
-  end
-
-  def create_movie
-  end
-
-  def new_movie
-  end
-
-  # BOOKS
-
-  def books_index
-  end
-
-  def create_book
-  end
-
-  def new_book
   end
 
   # LOGIN
@@ -45,5 +13,6 @@ class UsersController < ApplicationController
 
   def create_login
   end
+
 
 end
--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -11,5 +11,37 @@ class WorksController < ApplicationController
 
   def destroy
   end
-  
+
+  # ALBUMS
+
+  def albums_index
+  end
+
+  def create_album
+  end
+
+  def new_album
+  end
+
+  # MOVIES
+
+  def movies_index
+  end
+
+  def create_movie
+  end
+
+  def new_movie
+  end
+
+  # BOOKS
+
+  def books_index
+  end
+
+  def create_book
+  end
+
+  def new_book
+  end
 end
