1f1828f3028abf41ae8496e6417fb46269e5123a
natalia-ku <bilberryfm@mail.ru>
2017-04-12 10:46:07 -0700

    added create method for session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,19 @@
 class SessionsController < ApplicationController
 
   def new
-    
+
   end
 
   def create
-
+    user = User.find_by(username: params[:username])
+    if user
+      session[:user_id] = user.id
+      flash[:success] = "#{ user.username } is successfully logged in"
+      redirect_to root_path
+    else
+      flash[:failure] = "User wasn't logged in, try again"
+      render :new, status: :bad_request
+    end
   end
 
 end
