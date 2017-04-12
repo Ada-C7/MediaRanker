beaa823505dedee7343ede1023252fb968e5f5db
Tamiko Terada <tamikodevo@gmail.com>
2017-04-12 15:07:23 -0700

    create routes for media_type index and new

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,9 @@
 class WorksController < ApplicationController
+  def index
+    @media_type = params[:media_type]
+  end
+
+  def new
+    @work = Work.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,26 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/works', to: 'works#index', as: 'works'
-  get '/works/new', to: 'works#new', as: 'new_work'
-  post '/works', to: 'works#create'
-  get '/works/:id', to: 'works#show', as: 'work'
-  get '/works/:id/edit', to: 'works#edit', as: 'edit_work'
-  patch '/works/:id', to: 'works#update'
-  put '/works/:id', to: 'works#update'
-  delete '/works/:id', to: 'works#destroy'
 
-  # TODO URLS that exist in model site
-  # root welcome#index
-  # '/works/:id'
-  # '/works/:id/edit'
+  # welcome page shows top 10 of all three works
+  # root 'welcome#index'
+  # routes specifically needed for each media type
+
+  # get '/movies', to: 'works#movies', as: 'movies'
+  # get '/albums', to: 'works#albums', as: 'albums'
+  # get '/books', to: 'works#books', as: 'books'
+  get '/:media_type', to: 'works#index', as: 'works', constraints: { media_type: /(albums)|(movies)|(books)/ }
+
+  # match '/books/new', to: 'works#new', as: 'new_book'
+  # match '/albums/new', to: 'works#new', as: 'new_album'
+  # match '/movies/new', to: 'movies#new', as: 'new_movie'
+  get '/:media_type/:id', to: 'works#new', as: 'new_work', constraints: { media_type: /(albums)|(movies)|(books)/ }
+  # post '/works', to: 'works#create'
+  # get '/works/:id', to: 'works#show', as: 'work'
+  # get '/works/:id/edit', to: 'works#edit', as: 'edit_work'
+  # patch '/works/:id', to: 'works#update'
+  # delete '/works/:id', to: 'works#destroy'
+
   # '/login'
   # '/users'
   # '/users/:id'
-
-  # '/books'
-  # '/movies'
-  # '/albums'
-  # each route goes to different controller#action BUT render the same view page
-
-  # '/books/new'
-  # '/movies/new'
-  # '/albums/new'
-
 end
