fd86c28b43b923cef0ac2e43624651452ab6a6f9
Ann Dai <gofarann@gmail.com>
2017-04-12 15:00:08 -0700

    added sessions controller and added create method for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find_by id: params[:id]
+    if !@user
+      render_404
+    end
+  end
+
+  def create
+    @user = User.create! user_params
+    unless @user.id == nil
+      flash[:sucess] = "Successfully created new user #{@user.name} with ID #{@user.id}"
+    else
+      render "new" #should go back to login page
+    end
   end
 end
