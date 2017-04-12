f8a99b27fa685c6c819303b4956d562a935da592
Tamiko Terada <tamikodevo@gmail.com>
2017-04-12 15:37:23 -0700

    route root to Welcome#index

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # welcome page shows top 10 of all three works
-  # root 'welcome#index'
+  root 'welcome#index'
   get '/:media_type', to: 'works#index', as: 'works', constraints: { media_type: /(albums)|(movies)|(books)/ }
   get '/:media_type/:id', to: 'works#new', as: 'new_work', constraints: { media_type: /(albums)|(movies)|(books)/ }
   # post '/works', to: 'works#create'
