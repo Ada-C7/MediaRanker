3f5161d742ae37ec598609187c0a70cc1918f490
Ashtn <ashtonehorton@gmail.com>
2017-04-11 19:51:07 -0700

    created controller action skeleton"

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,27 @@
 class WorksController < ApplicationController
+
+def index
+end
+
+def edit
+end
+
+def show
+end
+
+def update
+end
+
+def destroy
+end
+
+def new
+end
+
+def create
+end
+
+def index
+end
+
 end
