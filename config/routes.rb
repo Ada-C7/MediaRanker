23176c988ad3d87b400ffabccd66e08aa93eb37f
Sofia Kim <sofiamhkim@gmail.com>
2017-04-12 15:15:01 -0700

    session controller created

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,17 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   #resources :users
-  # resources :works #add upvote controller
+  #resources :works #add upvote controller
   #
-  # resources :works, expect: [:index, :new, :create]
+
+   resources :works, expect: [:index, :new, :create]
   # resources :albums, only: [:index, :new, :create]
   # resources :movies
   # resources :books
   # resources :users, only: [:index, :show]
 
   # resources :welcome
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
