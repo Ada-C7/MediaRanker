10a10bc553b949dd2baa67c0acb12a90e115e965
add2point71dots <alcland@gmail.com>
2017-04-12 15:14:53 -0700

    setup users model and tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,29 @@ require "test_helper"
 describe User do
   let(:user) { User.new }
 
-  it "must be valid" do
-    value(user).must_be :valid?
+  it "can create a valid user" do
+    user.username = "User1"
+    user.must_be :valid?
+  end
+
+  it "users require a username" do
+    user.valid?
+    user.errors.messages.must_include :username
+  end
+
+  it "can't create user with a blank username" do
+    user.username = ""
+    user.valid?
+    user.errors.messages.must_include :username
+  end
+
+  it "user's username must be unique" do
+    user.username = "cool name"
+    user.save
+    new_user = User.new
+    new_user.username = "cool name"
+
+    new_user.valid?
+    new_user.errors.messages.must_include :username
   end
 end
