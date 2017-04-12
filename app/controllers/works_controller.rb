bbc676ab1dc4047412933abfd521976b675624d2
Hyunji Kim <ricecakemonster@gmail.com>
2017-04-12 16:30:42 -0700

    Added each work's list page

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,7 @@
 class WorksController < ApplicationController
+  def index
+    @work_category = params[:work_category]
+    category = @work_category.chomp!('s')
+    @works = Work.where(category: category)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,19 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'main#index', as: 'main'
   resources :users
-  resources :work
+  resources :works, except: [:index]
+
+
+  get 'works/:work_category/index', to: 'works#index', as: 'list_works'
+
+  # get 'works/new', to: 'works#new', as: 'new_work'
+  # post 'works', to: 'works#create'
+  # get 'works/:id', to: 'works#show', as: 'work'
+  # get 'works/:id/edit', to: 'works#edit', as: 'edit_work'
+  # patch 'works/:id', to: 'works#update'
+  # delete 'works/:id', to: 'works#destory'
+
+
 end
 
 
