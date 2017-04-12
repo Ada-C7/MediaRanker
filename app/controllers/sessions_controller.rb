4137033f1b42b308be59eb2489e0c534e39082cd
Regan Huff <regan.huff@gmail.com>
2017-04-12 15:39:47 -0700

    Add logout action and link from header

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,14 +10,20 @@ class SessionsController < ApplicationController
       #found successfully
       session[:user_id] = user.id
       flash[:success] = "Successfully logged in as existing user #{user.name}"
-      
       redirect_to root_path
+
     else
       #did not find; create new user with name from params
       new_user = User.create(name: params[:user][:name])
+      session[:user_id] = new_user.id
       flash[:success] = "Successfully created new user #{new_user.name} with ID #{new_user.id}"
-
       redirect_to root_path
     end
+  end
+
+  def logout
+    session[:user_id] = nil
+    flash[:success] = "Successfully logged out"
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
   get "/login", to: "sessions#login_form"
   post "/login", to: "sessions#login"
+  delete "/logout", to: "sessions#logout"
 
 
 
