5bf603c1b0b5b2bab4c854867816d08c362d4586
Ting Wong <hellotingwong@gmail.com>
2017-04-11 21:47:33 -0700

    first draft of user model tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,28 @@
-# require "test_helper"
-#
-# describe User do
-#   let(:user) { User.new }
-#
-#   it "must be valid" do
-#     value(user).must_be :valid?
-#   end
-# end
+require "test_helper"
+
+describe User do
+  describe "validations" do
+    it "is invalid without a username" do
+      user = User.new
+      result = user.valid?
+      result.must_equal false
+
+      user.errors.messages.must_include :username
+    end
+
+    it "creates user with username populated" do
+      user = User.create(username: "Ting")
+      user.id.wont_be_nil
+    end
+  end
+
+  ####################################################
+
+  # Not sure about whether this needs testing (or how to test has_manys)
+
+  describe "relationship" do
+    it "should have many votes" do
+    end
+  end
+  ####################################################
+end
