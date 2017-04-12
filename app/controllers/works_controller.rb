cddbbb405a46c1145ddb75b7125bd33d7d13f5a5
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-04-11 19:35:24 -0700

    created action controllers for works

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,22 +1,49 @@
 class WorksController < ApplicationController
   def index
     @works = Work.all
-    @books = Work.where(category: 'book')
-    @movies = Work.where(category: 'movie')
-    @albums = Work.where(category: 'album')
-  end
-
-  def edit
   end
 
   def show
     @result_work = Work.find(params[:id])
   end
 
+  def edit
+    @work = Work.find(params[:id])
+  end
+
   def update
+    @work = Work.find(params[:id])
+
+    @work.category = work_params[:category]
+    @work.title = work_params[:title]
+    @work.creator = work_params[:creator]
+    @work.year = work_params[:year]
+    @work.description = work_params[:description]
+
+    if @work.save
+      redirect_to work_path(@work.id)
+    else
+      render "edit"
+    end
+
+  end
+
+  def books
+    @books = Work.where(category: 'book')
+  end
+
+  def movies
+    @movies = Work.where(category: 'movie')
+  end
+
+  def albums
+    @albums = Work.where(category: 'album')
   end
 
   def destroy
+    Work.destroy(params[:id])
+
+    redirect_to root_path
   end
 
   private
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170411192738) do
+ActiveRecord::Schema.define(version: 20170412021523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 20170411192738) do
     t.string   "creator"
     t.integer  "year"
     t.text     "description"
-    t.string   "mediatype"
+    t.string   "category"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
