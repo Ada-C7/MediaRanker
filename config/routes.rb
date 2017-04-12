c986f50acb59595e500236de4a7c31d3b6178a5b
Tamiko Terada <tamikodevo@gmail.com>
2017-04-12 15:28:47 -0700

    add welcome controller for landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,11 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # welcome page shows top 10 of all three works
   # root 'welcome#index'
-  # routes specifically needed for each media type
-
-  # get '/movies', to: 'works#movies', as: 'movies'
-  # get '/albums', to: 'works#albums', as: 'albums'
-  # get '/books', to: 'works#books', as: 'books'
   get '/:media_type', to: 'works#index', as: 'works', constraints: { media_type: /(albums)|(movies)|(books)/ }
-
-  # match '/books/new', to: 'works#new', as: 'new_book'
-  # match '/albums/new', to: 'works#new', as: 'new_album'
-  # match '/movies/new', to: 'movies#new', as: 'new_movie'
   get '/:media_type/:id', to: 'works#new', as: 'new_work', constraints: { media_type: /(albums)|(movies)|(books)/ }
   # post '/works', to: 'works#create'
   # get '/works/:id', to: 'works#show', as: 'work'
