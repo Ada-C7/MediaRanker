45ca3223c41c4221d4491e8fe95ba1ba6b8eae5f
Anna Elisabeth <anna.e.boyd@gmail.com>
2017-04-12 11:44:37 -0700

    added through connectors in model

--- app/models/work.rb
+++ app/models/work.rb
@@ -3,4 +3,5 @@ class Work < ApplicationRecord
   validates :category, presence: true, inclusion: { in:%w(album movie book),
     message: "%{value} is not a valid category" }
   has_many :votes
+  has_many :users, through: :votes
 end
