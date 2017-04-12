1f941f7beb3c650ec7566bf1d8d3958244159bd7
Haby Randall <sow.haby@gmail.com>
2017-04-12 00:18:43 -0700

    First Testing in model

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,9 +1,26 @@
 require "test_helper"
 
 describe Work do
-  let(:work) { Work.new }
+  let(:work) { works(:duck) }
 
-  it "must be valid" do
-    value(work).must_be :valid?
+  it "requires a title" do
+    # work = works(:owl)
+    work.valid?.must_equal true
+  end
+
+  it "requires a creator" do
+    work.valid?.must_equal true
+  end
+
+  it "requires a unique title" do
+
+  end
+
+  it "requires that category be only 'book', 'movie', or 'album'" do
+
+  end
+
+  it "requires that a publish_year attribute is present" do
+
   end
 end
