0b615e6ff2d3b9696a9b9ef421443601821c2d2e
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-04-11 17:19:25 -0700

    have back links working inin edit view

--- app/helpers/works_helper.rb
+++ app/helpers/works_helper.rb
@@ -1,2 +1,12 @@
 module WorksHelper
+  # only available in your views
+  def find_path(work)
+    if work.category == "movie"
+      return movies_path
+    elsif work.category == "book"
+      return books_path
+    elsif work.category == "album"
+      return albums_path
+    end
+  end
 end
