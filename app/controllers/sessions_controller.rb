1df3dc82591d8df6c762f1a86a5c21cc5dca75e2
Lynn Trickey <lynn.trickey@gmail.com>
2017-04-12 14:24:44 -0700

    pausing to switch branches and create user model first

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,10 @@
 class SessionsController < ApplicationController
 
-  def login_form
-  end
+  def login_form; end
 
   def login
+    # need to create user model first!
+    # user = User.find_by_name
   end
 
 end
