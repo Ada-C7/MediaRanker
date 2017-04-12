8fb3e621382ba85f8501b3bc7d61444b4d0cd738
Anacelia Gillette <acgillette426@gmail.com>
2017-04-12 15:03:45 -0700

    Added more controller methods: update and destroy for albums

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -11,4 +11,26 @@ class AlbumsController < ApplicationController
     @album = Content.find(params[:id])
   end
 
+  def update
+    @album = Content.find(params[:id])
+
+    if @album.update album_params
+      redirect_to album_path
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    Content.destroy(params[:id])
+
+    redirect_to albums_path
+  end
+
+  private
+
+  def album_params
+    params.require(:content).permit(:title, :creator, :publication_year, :description)
+  end
+
 end
