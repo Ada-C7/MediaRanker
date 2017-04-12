3c52208451db1167b3f1b2b1fd2b4cde02a8008a
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-04-11 19:04:19 -0700

    removed controllers

--- app/controllers/albums_controller.rb
+++
@@ -1,5 +0,0 @@
-class AlbumsController < ApplicationController
-  def index
-    @albums = Work.where(mediatype: 'album')
-  end
-end
--- test/controllers/movies_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-describe MoviesController do
-  it "should get index" do
-    get movies_index_url
-    value(response).must_be :success?
-  end
-
-end
