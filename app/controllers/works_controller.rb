4d41ca63104096349f6c35c577f628f32d1c078d
jjousun <jjousun@gmail.com>
2017-04-11 17:29:29 -0700

    Working on form for new

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -15,4 +15,49 @@ class WorksController < ApplicationController
     return @category_works
   end
 
+  def edit
+    @work = Work.find_by_id(params[:id])
+  end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work = Work.create work_params
+
+    if @work.id != nil
+      redirect_to works_path
+    else
+      render "new"
+    end
+  end
+
+  def update
+    @work = Work.find_by_id(params[:id])
+
+    @work.category = work_params[:category]
+    @work.title = work_params[:title]
+    @work.creator = work_params[:creator]
+    @work.publication_year = work_params[:publication_year]
+    @work.description = work_params[:description]
+
+    if @work.save
+      redirect_to category_path(params[:category])
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    Work.destroy(params[:id])
+
+    redirect_to works_path
+  end
+
+  private
+
+  def work_params
+    params.require(:work).permit(:category, :title, :author_id, :publication_year, :description)
+  end
 end
