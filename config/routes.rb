cf24584b30987f23a1791dbd562a5476bbc2431d
Janice Lichtman <jalichtman@gmail.com>
2017-04-13 01:51:37 -0700

    made my items routes restful

--- config/routes.rb
+++ config/routes.rb
@@ -5,21 +5,19 @@ Rails.application.routes.draw do
 
   root 'items#index'
 
-  get 'items', to:'items#index'
-  get 'items/:id', to:'items#show', as: 'item'
-  get 'items/:id/edit', to:'items#edit', as: 'edit_item'
-  patch 'items/:id', to:'items#update'
-  delete 'items/:id', to:'items#destroy'
+
+  resources :items, except: [:new, :create]
+
+
+  # get 'items', to:'items#index'
+  # get 'items/:id', to:'items#show', as: 'item'
+  # get 'items/:id/edit', to:'items#edit', as: 'edit_item'
+  # patch 'items/:id', to:'items#update'
+  # delete 'items/:id', to:'items#destroy'
 
   get ':category', to: 'items#index_by_category', as: 'category_index'
 
   get ':category/new',   to:'items#new', as:'new_item_by_category'
   post ':category',   to:'items#create'
 
-
-
 end
-
-#resources :books, controller: 'items', category "book"
-
-  #resources :items
