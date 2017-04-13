616a8a55793e0e1ad6fe266f3b24c9c4409f8774
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-04-12 20:44:20 -0700

    tests passing for users validations

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,5 +1,5 @@
 class Work < ApplicationRecord
-  has_many :votes
+  # has_many :votes
   validates :title, presence: true
   validates :creator, presence: true
   validates :publication_year, presence: true, numericality: { only_integer: true }
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,25 @@
 require "test_helper"
 
 describe User do
-  let(:user) { User.new }
+  describe 'validations' do
 
-  it "must be valid" do
-    value(user).must_be :valid?
+    let(:user) { User.new(username: "Finding Nemo")}
+
+    it 'passes when given good data' do
+      user.valid?.must_equal true
+    end
+
+    it 'does not save new user when given bad data' do
+      user = User.new()
+      user.valid?.must_equal false
+      user.errors.messages.must_include :username
+    end
+
+    it 'must be given unqiue user name' do
+      user.save
+      user2 = User.new(username: "Finding Nemo")
+      user2.valid?.must_equal false
+      user2.errors.messages.must_include :username
+    end
   end
 end
