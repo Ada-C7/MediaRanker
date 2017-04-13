156c7504d64863af56e6e864e2b7fbbca8b1f997
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-04-12 21:29:46 -0700

    working on log in function working for established users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,13 +8,24 @@ class SessionsController < ApplicationController
     if user
       session[:user_id] = user.id
       flash[:success] = "#{ user.username } is successfully logged in"
-      redirect_to root_path
+      redirect_to :homepage
     end
   end
+
+  # method copied from textbook
+  # def login
+  #   user = User.find_by(username: params[:username])
+  #
+  #   if user
+  #     session[:user_id] = user.id
+  #     flash[:success] = "#{ user.name } is successfully logged in"
+  #     redirect_to root_path
+  #   end
+  # end
 
   def destroy
     session[:user_id] = nil
     flash[:logout] = "Logged Out"
-    redirect_to root_path
+    redirect_to :homepage
   end
 end
