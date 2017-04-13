65dacc7ce605d85ed38be176649f97b9e9956a36
EricaJCase <ericajcase@gmail.com>
2017-04-12 21:03:48 -0700

    first controller test passed

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,6 +2,7 @@ require "test_helper"
 
 describe UsersController do
     it "it can get index page" do
+      User.count.must_be :>, 0
       get users_path
       must_respond_with :success
     end
