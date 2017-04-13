d6233b71659cf5acc6b7d3d5776437a0d96e5818
Chantal Rollison <rollison.chantal@gmail.com>
2017-04-12 18:27:17 -0700

    login button almost successful, need to assign user_id

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def login_form; end
 
-  def login_form
+  def login
     # need to give them user id
     flash[:success] = "Login successful!"
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170411201238) do
+ActiveRecord::Schema.define(version: 20170413012017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20170411201238) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "username"
   end
 
   create_table "votes", force: :cascade do |t|
