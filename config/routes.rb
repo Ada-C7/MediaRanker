1c7c1b921fd0c9d9cd2c2ba0534f3911a1f6b40c
Ashtn <ashtonehorton@gmail.com>
2017-04-12 21:08:23 -0700

    created temp upvote route

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ resources :works, except: [:new, :create]
 get '/:category/new', to: 'works#new', as: 'new_work'
 get '/:category', to: 'works#show_category', as: 'category'
 post '/:category', to: 'works#create'
+post '/work/:id', to: 'works#upvote', as: 'upvote'
 end
