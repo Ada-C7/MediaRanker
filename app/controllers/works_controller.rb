11b89414c384bd5b84016a4924785cceafca15a9
Marisol Lopez <lmarisolopez@gmail.com>
2017-04-12 19:38:54 -0700

    wrote code to loop through worksindex.html.erb page by category based on url

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,6 +1,6 @@
 class WorksController < ApplicationController
   def index
-    @works = Work.by_category(params[:category])
+    @works = Work.by_category(params[:category].singularize)
   end
   def show
     @work = Work.find(params[:id])
