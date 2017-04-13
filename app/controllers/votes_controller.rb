0f5153b0057b4e304ce72fa6807f6cd821bf8df6
Alix Hamilton <alix.hamilton@gmail.com>
2017-04-13 15:50:03 -0700

    Doesn't allow user to vote on a work twice

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,16 +1,20 @@
 class VotesController < ApplicationController
   def upvote
-    if session[:user_id]
-      work = Work.find_by_id(params[:id])
+    work = Work.find_by_id(params[:id])
+    user = User.find_by_id(session[:user_id])
+
+    if !session[:user_id]
+      flash[:error] = "You must be logged in to vote."
+    elsif user.votes.map { |vote| vote.work_id }.include? work.id
+      flash[:error] = "You already voted for #{work.title}."
+    else
       vote = Vote.new(
         work_id: work.id,
-        user_id: session[:user_id]
+        user_id: user.id
       )
 
       vote.save
       flash[:success] = "Successfully upvoted!"
-    else
-      flash[:error] = "You must be logged in to vote."
     end
 
     redirect_to :back
