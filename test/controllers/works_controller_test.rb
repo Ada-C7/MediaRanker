9a617e0b78fb0bfca525107d31da2148ec8bd926
Tamiko Terada <tamikodevo@gmail.com>
2017-04-13 10:01:04 -0700

    test(controller)-add specs for Work#new

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -1,23 +1,39 @@
 require "test_helper"
 
 describe WorksController do
-  it "should get index for books" do
-    get works_path(media_type: "books")
-    must_respond_with :success
+  describe 'Works#index' do
+    it "should get index for books" do
+      get works_path(media_type: "books")
+      must_respond_with :success
+    end
+
+    it "should get index for albums" do
+      get works_path(media_type: "albums")
+      must_respond_with :success
+    end
+
+    it "should get index for books" do
+      get works_path(media_type: "movies")
+      must_respond_with :success
+    end
+
+    it "should raise error with invalid media_type " do
+      proc {
+        get works_path(media_type: "panda")}.must_raise ActionController::UrlGenerationError
+    end
   end
 
-  it "should get index for albums" do
-    get works_path(media_type: "albums")
-    must_respond_with :success
-  end
+  describe 'Works#new' do
+    it "should get new_book for books" do
+      get new_work_path(media_type: "books")
+    end
 
-  it "should get index for books" do
-    get works_path(media_type: "movies")
-    must_respond_with :success
-  end
+    it "should get new_book for books" do
+      get new_work_path(media_type: "movies")
+    end
 
-  it "should fail with invalid media_type " do
-    proc {
-      get works_path(media_type: "panda")}.must_raise ActionController::UrlGenerationError
+    it "should get new_book for books" do
+      get new_work_path(media_type: "albums")
+    end
   end
 end
