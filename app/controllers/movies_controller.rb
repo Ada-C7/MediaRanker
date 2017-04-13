7fd6167262c4a130560864c8595ea8a7f7340221
Anacelia Gillette <acgillette426@gmail.com>
2017-04-13 15:19:49 -0700

    Movies: index and show controller and controller tests green

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,9 @@
 class MoviesController < ApplicationController
   def index
+    @movies = Content.movies
   end
 
   def show
+    @movie = Content.find(params[:id])
   end
 end
--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,14 +1,15 @@
 require "test_helper"
 
 describe MoviesController do
-  # it "should get index" do
-  #   get movies_path
-  #   must_respond_with :success
-  # end
-  #
-  # it "should get show" do
-  #   get movie_path
-  #   must_respond_with :success
-  # end
+
+  it "should get index" do
+    get movies_path
+    must_respond_with :success
+  end
+
+  it "should get show" do
+    get movie_path(contents(:shrek).id)
+    must_respond_with :success
+  end
 
 end
