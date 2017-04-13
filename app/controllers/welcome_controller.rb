eea637964bdfe98476087a2282798e0a5df28060
Elizabeth Deutsch <elizabeth.deutsch@att.net>
2017-04-13 16:02:40 -0700

    commented out @movies in welcome controller index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   def index
     # @works = Work.all
-    @movies = Work.gather_movies
+    # @movies = Work.gather_movies
     @books = Work.gather_books
     @albums = Work.gather_albums
   end
