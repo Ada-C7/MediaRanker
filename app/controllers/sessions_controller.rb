e3f00e673d1cc13453a5a8e9b2a460133e5fae63
laura <lmelgarejos@unal.edu.co>
2017-04-13 12:51:11 -0700

    Added session when user is logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,9 +9,10 @@ class SessionsController < ApplicationController
     if @user
       session[:user_id] = @user.id
       flash[:succes] = "#{@user.name} is successfully logged in"
+      session[:user_name] = "Logged in as #{@user.name}"
       redirect_to root_path
     else
-      flash[:no_user_found] = "Name does not match any user's name, please try it again."
+      flash[:no_user_found] = "Name does not match any user's name (case sensitive!), please try it again, or create a new user."
       redirect_to login_path
     end
   end
