331663b1e47926398f38233dbbce90c243d180a4
Chantal Rollison <rollison.chantal@gmail.com>
2017-04-12 20:03:26 -0700

    fixed user logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,9 @@ class SessionsController < ApplicationController
   def login_form; end
 
   def login
+
     user_to_login = User.find_or_create(params[:username])
+    session[:user_id] = user_to_login.id
     flash[:success] = "Logged in as #{ user_to_login.username }!"
     redirect_to root_path
   end
