e50a20e823f430d4862cf5929aff7bb0459a1757
Anacelia Gillette <acgillette426@gmail.com>
2017-04-13 15:44:22 -0700

    Added index page with all content

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,4 +1,7 @@
 class ContentsController < ApplicationController
   def index
+    @albums = Content.albums
+    @books = Content.books
+    @movies = Content.movies
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+
+  root 'contents#index'
+
   resources :books
   resources :movies
   resources :albums
 
-  get 'contents/index'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
