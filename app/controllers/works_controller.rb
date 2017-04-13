e9576a69f6efa7a2823ae9714e1cf5ea86ed42e9
amb54 <ambarklund@hotmail.com>
2017-04-13 08:48:20 -0700

    Created users and votes in seeds.rb. Added a limit of 10 for the instance variables in index for the work controller. Added the links to the show pages for the works in the index view for the works.

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,8 +1,8 @@
 class WorksController < ApplicationController
   def index
-    @movies = Work.where(category: "movie")
-    @books = Work.where(category: "book")
-    @albums = Work.where(category: "album")
+    @albums = Work.where(category: "album").limit(10)
+    @books = Work.where(category: "book").limit(10)
+    @movies = Work.where(category: "movie").limit(10)
   end
 
   def albums
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,9 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 require 'csv'
+# require 'faker'
 
-
-#WORK
+# WORK
 csv_text = File.read(Rails.root.join('lib', 'seeds', 'media_seeds.csv'))
 csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
 csv.each do |row|
@@ -20,4 +20,23 @@ csv.each do |row|
   w.publication_year = row['publication_year'].to_i
   w.description = row['description']
   w.save
+end
+
+# USERS
+# 25.times do
+#   User.create(name: Faker::Name.first_name)
+# end
+
+User.create(name: "Anna")
+User.create(name: "Cara")
+User.create(name: "Elisabeth")
+User.create(name: "Maria")
+User.create(name: "Marisol")
+User.create(name: "Tehut")
+User.create(name: "Danielle")
+
+
+# VOTES
+100.times do
+  Vote.create(user_id: Random.rand(7), work_id: Random.rand(26))
 end
