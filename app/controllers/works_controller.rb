bdcacfebf1c3e3594757cbb05615a6ca53e9d49f
Tamiko Terada <tamikodevo@gmail.com>
2017-04-12 20:48:42 -0700

    works#index controller method and view

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,9 +1,16 @@
 class WorksController < ApplicationController
   def index
     @media_type = params[:media_type]
+    @albums = Work.albums
+    @books = Work.books
+    @movies = Work.movies
   end
 
   def new
     @work = Work.new
+  end
+
+  def self.books
+
   end
 end
