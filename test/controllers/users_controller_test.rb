bff07ad9fee102b3448a6ed95638adbf18b7638c
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-04-12 20:48:34 -0700

    wrote tests for users controller index

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 describe UsersController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe 'index' do
+
+    it "gets the index page successfully" do
+      User.count.must_be :>, 0
+      get users_path
+      must_respond_with :success
+    end
+
+    it 'still successful when there are no works' do
+      User.destroy_all
+      get users_path
+      must_respond_with :success
+    end
+
+  end
 end
