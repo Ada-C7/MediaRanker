073b53f0ccad54476221a5598a6df1171d93121c
jjousun <jjousun@gmail.com>
2017-04-12 18:58:20 -0700

    Created login

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
   resources :votes, except: [:destroy, :edit, :update]
   resources :users, except: [:destroy, :edit, :update]
 
+  get "/login", to: "sessions#login_form"
+  post "/login", to: "sessions#login"
+
+  delete "/logout", to: "sessions#logout"
+
   get "/:category/new", to: "works#new", as: "new_category"
   post "/:category", to: "works#create"
 
