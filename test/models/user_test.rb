8e2a7459a764b9cf8ce2149713140af66ceb2ff4
Jackie Watanabe <sushiigirl@gmail.com>
2017-04-13 00:16:08 -0700

    added tests to user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,34 @@ require "test_helper"
 describe User do
   let(:user) { User.new }
 
-  it "must be valid" do
-    value(user).must_be :valid?
+  it "Users must have names" do
+    user.valid?.must_equal false
+    #hash of all message
+    user.errors.messages.must_include :name
+  end
+
+  it "If a name is given, user is valid" do
+    user.name = "Name"
+    user.valid?.must_equal true
+
+    user.errors.messages[:name].must_equal []
+  end
+
+  it "you can create a user" do
+    user.name = "Name"
+
+    user.valid?.must_equal true
+  end
+
+  it "Names must be unique" do
+    user1 = User.new
+    user1.name = "Jackie"
+    user1.save
+
+    user2 = User.new
+    user2.name = "Jackie"
+
+    user2.valid?.must_equal false
+    user2.errors.messages[:name].must_equal ["has already been taken"]
   end
 end
