6b2a3eb87ae1d2c5703b39889d4f37b0ea7dca14
janicewilson <janice.wilson@mac.com>
2017-04-13 16:37:41 -0700

    Fixed users: Distinguishes new visitors from return visitors. Creates a new user and login for the former, matches usernames for the latter.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,23 @@
 class SessionsController < ApplicationController
 
-     def login_form; end
-
-     def login
-          user = User.find_by(params[:username])
-
-               if user
-                    flash[:success] = "#{user.username}, you're on. Go forth and vote."
-                    session[:user_id] = user.id
-                    redirect_to root_path
-               else
-                    flash.now[:error] = "Ugh. Not even. If you're a real human being, try again."
-                    render :form
-               end
+     def login_form
+          @user = User.new
      end
 
-     def logout_form; end
+     def login
+          user = User.find_by_username(params[:username])
+
+               if user
+                    flash[:success] = "#{user.username}, #{user.id} Welcome back. Go forth and vote."
+                    session[:user_id] = user.id
+                    redirect_to user_path(3)
+               else
+                    user = User.create(username: params[:username], status: true)
+                    flash[:success] = "#{user.username}, #{user.id} You're on for the first time. Use your votes wisely."
+                    session[:user_id] = user.id
+                    redirect_to user_path(3)
+               end
+     end
 
      def logout
           session.delete(:user_id)
