091a88f414ec0198d103474b8511149b00e95185
EricaJCase <ericajcase@gmail.com>
2017-04-12 21:26:03 -0700

    index passes the tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,15 +1,18 @@
 require "test_helper"
 
 describe UsersController do
+
+  describe 'index' do
     it "it can get index page" do
       User.count.must_be :>, 0
       get users_path
       must_respond_with :success
     end
 
-    # it "still responds successfully when there are no users" do
-    #   User.destroy_all
-    #   get user_path
-    #   must_respond_with :success
-    # end
+    it "still responds successfully when there are no users" do
+      User.destroy_all
+      get users_path
+      must_respond_with :success
+    end
+  end
 end
