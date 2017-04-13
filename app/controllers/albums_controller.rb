4bcf2dbd27888ff2d8fd1812217a5bc0a258c9fe
Chantal Rollison <rollison.chantal@gmail.com>
2017-04-13 15:13:34 -0700

    added links on details page and index pages

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -7,4 +7,51 @@ class AlbumsController < ApplicationController
   def show
     @result_album = Work.find(params[:id])
   end
+
+  def edit
+    @album = Work.find(params[:id])
+  end
+
+  def update
+    @album = Work.find(params[:id])
+
+    @album.title = album_params[:title]
+    @album.creator = album_params[:creator]
+    @album.pub_year = album_params[:pub_year]
+    @album.description = album_params[:description]
+
+    if @album.save
+      redirect_to album_path(@album.id)
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    Work.destroy(params[:id])
+
+    redirect_to albums_path
+  end
+
+  def create
+    @album = Work.create album_params
+
+    if @album.id != nil
+      redirect_to albums_path
+    else
+      render "new"
+    end
+  end
+
+  def new
+    @album = Album.new
+  end
+
+  private
+
+    def album_params
+      params.require(:album).permit(:title, :creator, :pub_year, :description)
+    end
+
+
 end
--- app/controllers/media_ranker_controller.rb
+++ app/controllers/media_ranker_controller.rb
@@ -3,6 +3,6 @@ class MediaRankerController < ApplicationController
   def index
     @albums = Work.where(category: "album").limit(10)
     @books = Work.where(category: "book").limit(10)
-    # @movies = Work.where(category: "movie").limit(10)
+    @movies = Work.where(category: "movie").limit(10)
   end
 end
