f3a6bf19809478bc80484cdac28489fbb0051b93
Kaitlin Ramirez <rmrz.kt@gmail.com>
2017-04-13 12:43:43 -0700

    user form and user routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
   get 'books', to:'works#book_index', as: 'books'
 
   get 'works/:id', to:'works#show', as: 'work'
+
+  get 'users/new', to:'users#new', as:'new_user'
+  post 'users', to:'users#create'
+  get 'users/:id', to:'users#show', as:'user'
 end
