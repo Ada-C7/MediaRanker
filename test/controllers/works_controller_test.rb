9c141dea6d5528599bfcad407a989b5e567b1bf2
Tamiko Terada <tamikodevo@gmail.com>
2017-04-12 21:41:25 -0700

    TT(model)-add works#index tests

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 describe WorksController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  it "should get index for books" do
+    get works_path(media_type: "books")
+    must_respond_with :success
+  end
+
+  it "should get index for albums" do
+    get works_path(media_type: "albums")
+    must_respond_with :success
+  end
+
+  it "should get index for books" do
+    get works_path(media_type: "movies")
+    must_respond_with :success
+  end
+
+  it "should fail with invalid media_type " do
+    proc {
+      get works_path(media_type: "panda")}.must_raise ActionController::UrlGenerationError
+  end
 end
