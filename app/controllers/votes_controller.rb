77cdd84cc28c0ce1cac4b4a6722b1e981e657a03
Lynn Trickey <lynn.trickey@gmail.com>
2017-04-13 13:00:29 -0700

    Added ability to vote for media

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,6 +4,9 @@ class VotesController < ApplicationController
       vote = Vote.new
       vote.work_id = params[:id]
       vote.user_id = session[:user_id]
+      if vote.save
+        redirect_to work_path
+      end
     else
       flash[:warning] = "You must be logged in to Vote"
       redirect_to login_path
