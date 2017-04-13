1762fc0306c6ddf14ed6c8a43729bc92da6f3341
jjousun <jjousun@gmail.com>
2017-04-12 18:04:11 -0700

    Changed routes to go from most specific to most general so that the users index page shows up correctly

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "mediaranker#index"
 
+  resources :works
+  resources :votes
+  resources :users
+
   get "/:category/new", to: "works#new", as: "new_category"
   post "/:category", to: "works#create"
 
   get "/:category", to: "works#show_category", as: "category"
-
-  resources :works
-  resources :votes
-  resources :users
 end
