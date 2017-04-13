c81dd1b1c67168330b937fb3cb2e9983e7423810
Rana Sulaiman <rs_sulaiman@hotmail.com>
2017-04-12 19:09:42 -0700

    Work model validations passed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,12 @@
 
   def login
     user = User.find_by_name(params[:name])
-
+    # raise
     if user
       # found successfully
       session[:user_id] = user.id
       flash[:success] = "HELLO #{ user.name }"
-      redirect_to root_path
+      redirect_to works_path
     else
       # did not find
       flash.now[:error] = "User not found"
@@ -21,12 +21,6 @@
     # session[:user_id] = nil
     session.delete(:user_id)
     flash[:success] = "You are successfully logged out"
-    redirect_to root_path
+    redirect_to work_path
   end
-end
-
-
-
-
-
 end
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 describe Work do
   let(:work1) { Work.new }
 
-# Testing Validations  
+# Testing Validations
   it "Work must be valid" do
     # album1.category = "album"
     work1.title = "My Life"
@@ -16,6 +16,12 @@ describe Work do
 
     work1.errors.messages.must_include :title
   end
+
+  it "Can craete a medium/movie" do
+
+    works(:movie1).valid?.must_equal true
+  end
+
 
   it "If a title is given the movie is valid" do
     work1.title = "Neemo"
