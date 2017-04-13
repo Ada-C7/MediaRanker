082f18e6971685ffd1e11d0f5b6644069952670f
add2point71dots <alcland@gmail.com>
2017-04-12 23:30:24 -0700

    made users index page

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,14 @@ Rails.application.routes.draw do
 
   resources :works, except: :new
 
+  get "/users", to: "users#index"
+  get "/users/:id", to: "users#show", as: "user"
+
   get "/login", to: "sessions#login_form"
   post "/login", to: "sessions#login"
 
   delete "/logout", to: "sessions#logout", as: "logout"
 
-  get '/:category/new', to: 'works#new', as: 'new_work'
-  get '/:category', to: 'works#show_category', as: 'category'
+  get "/:category/new", to: 'works#new', as: "new_work"
+  get "/:category", to: "works#show_category", as: "category"
 end
