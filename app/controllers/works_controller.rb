9f67e0b5861a6e3dc92cb75d58d3e8d20a9ef632
Tamiko Terada <tamikodevo@gmail.com>
2017-04-12 20:49:28 -0700

    works#index controller method changes

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,16 +1,17 @@
 class WorksController < ApplicationController
   def index
-    @media_type = params[:media_type]
-    @albums = Work.albums
-    @books = Work.books
-    @movies = Work.movies
+    media_type = params[:media_type]
+    case media_type
+    when 'movies'
+      @works = Work.movies
+    when 'books'
+      @works = Work.books
+    when 'albums'
+      @works = Work.albums
+    end
   end
 
   def new
     @work = Work.new
-  end
-
-  def self.books
-
   end
 end
