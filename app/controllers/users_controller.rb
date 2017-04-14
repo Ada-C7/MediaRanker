a812cdcf2f2a3977bb38d7771b3384becb6cb8b1
Tehut Getahun <tehut.m.g@gmail.com>
2017-04-14 01:10:30 -0700

    added views and controller log to for join button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,33 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+
+    if @user.save
+      flash[:welcome] = "Welcome to TehutRanker #{params[:username]}!"
+      redirect_to users_path
+    else
+      redirect_to new_users_path
+      flash[:failure] = "We could not create your account"
+    end
+
+  end
 
   def show
     @user = User.find(params[:id])
+  end
+
+
+  private
+  def user_params
+    return params.require(:user).permit(:username)
   end
 end
