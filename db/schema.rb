04bccc3dca5b8591a04d70386b95b7a9091be016
Ashtn <ashtonehorton@gmail.com>
2017-04-14 11:47:51 -0700

    branch fixed yay!

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170414143926) do
+ActiveRecord::Schema.define(version: 20170414172316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,8 +22,12 @@ ActiveRecord::Schema.define(version: 20170414143926) do
   end
 
   create_table "votes", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "work_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_votes_on_user_id", using: :btree
+    t.index ["work_id"], name: "index_votes_on_work_id", using: :btree
   end
 
   create_table "works", force: :cascade do |t|
