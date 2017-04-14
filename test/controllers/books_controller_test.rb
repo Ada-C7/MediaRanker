efd6617dfc9f0ccccba4bd99cd03c195819a16d5
Theresa Manney <theresa.m.17@gmail.com>
2017-04-14 15:31:32 -0700

    finished stubbing out controller tests...created a few too

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -2,7 +2,17 @@ require "test_helper"
 
 describe BooksController do
   it "should get index" do
-
+    get books_path
+    must_respond_with :success
   end
 
+  it "should show the new book form" do
+  end
+
+  it "should redirect to book list after adding a work" do
+  end
+
+  it "should effect the work model when creating a work" do
+    # maybe this only needs to be tested in works controller?
+  end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,7 +2,18 @@ require "test_helper"
 
 describe UsersController do
   it "should get index" do
-
+    get users_path
+    must_respond_with :success
   end
 
+  it "should get show page" do
+    get user_path(users(:one).id)
+    must_respond_with :success
+  end
+
+  it "should show a 404 when user not found" do
+    skip
+    get user_path(3)
+    must_respond_with :missing
+  end
 end
