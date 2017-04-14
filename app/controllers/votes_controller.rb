eb43a537097a974e171fc1ea30b800ebb48f1463
Chantal Rollison <rollison.chantal@gmail.com>
2017-04-13 17:35:22 -0700

    upvotes works

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,11 +1,24 @@
 class VotesController < ApplicationController
 
   def upvote
-    params[:work_id]
-    @vote = Vote.create
+    work_id = params[:work_id]
+
+    user_id =  session[:user_id]
+
+    @vote = Vote.create(work_id: work_id, user_id: user_id)
 
     if @vote.id != nil
-      redirect_to votes_path
+      voted_media = Work.find(work_id)
+
+      if voted_media.category == "album"
+        redirect_to albums_path
+      elsif voted_media.category == "book"
+        redirect_to books_path
+      elsif voted_media.category == "movie"
+        redirect_to movies_path
+      else
+        redirect_to root_path
+      end
     end
   end
 
