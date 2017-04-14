b8104a597eb36b5389984f1e690ca5128879a84c
AlisonZ <alisonkzerbe@gmail.com>
2017-04-13 18:15:41 -0700

    voting is working and only allowed once per person per item

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,15 +1,20 @@
 class VotesController < ApplicationController
     def upvote
-        vote = Vote.new
-        vote[:work_id] = params[:id]
-        vote[:user_id] = session[:user_id]
-
-        if vote.save
-            flash[:success] = "Thanks for voting"
+        if Vote.already_created(params[:id], session[:user_id] )
+            flash[:error] = "You've already voted for this"
             redirect_to root_path
         else
-            flash[:error] = "Your vote has not been counted"
-            redirect_to login_path
+            vote = Vote.new
+            vote[:work_id] = params[:id]
+            vote[:user_id] = session[:user_id]
+
+            if vote.save
+                flash[:success] = "Thanks for voting"
+                redirect_to root_path
+            else
+                flash[:error] = "Your vote has not been counted"
+                redirect_to login_path
+            end
         end
     end
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,9 @@
 class Vote < ApplicationRecord
     belongs_to :work
     belongs_to :user
+
+
+    def self.already_created (work_id, user_id)
+        Vote.where(user_id: user_id, work_id: work_id).present?
+    end
 end
