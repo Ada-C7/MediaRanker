42dc2dc55fbfc367bada48c7da373273755f056a
Haby Randall <sow.haby@gmail.com>
2017-04-14 10:33:26 -0700

    Working on user index and show page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,19 +3,28 @@ class SessionsController < ApplicationController
 def login_form; end
 
 def login
-user = User.find_by_name(params[:name])
+@user = User.find_by_name(params[:name])
 
-if user
-session[:user_id] = user.id
-  flash[:success] = "HELLO #{ user.name }"
-  redirect_to root_path
+if @user
+session[:user_id] = @user.id
+  flash[:success] = "HELLO #{ @user.name }"
+  redirect_to user_path(@user.id)
 else
   #did not find
-  user = User.new
-  user.id = params[:id]
-  user.save
+  @user = User.create(user_params)
+  flash[:success] = "User has been added"
+  session[:user_id] = @user.id
+  redirect_to user_path(@user.id)
   # flash.now[:error] = "User not found"
   # render :login_form
 end
+end
+
+
+private
+
+def user_params
+  params.permit(:name)
+
 end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,21 +8,23 @@ class UsersController < ApplicationController
     @result_user = User.find(params[:id])
   end
 
-  def create
-    @user = User.new(user_params)
-
-    if @user.save
-      flash[:success] = "You are logged in"
-      redirect_to @user
-    else
-      render 'login'
-    end
-  end
-
-  private
-
-def user_params
-  params.require(:user).permit(:name)
-
+def create
 end
+#   def create
+#     @user = User.new(user_params)
+#
+#     if @user.save
+#       flash[:success] = "You are logged in"
+#       redirect_to @user
+#     else
+#       render 'login'
+#     end
+#   end
+#
+#   private
+#
+# def user_params
+#   params.require(:user).permit(:name)
+#
+# end
 end
