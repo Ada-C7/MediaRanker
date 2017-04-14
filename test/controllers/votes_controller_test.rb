95d02ee0e224420258c785d3ff75e30e48b43cab
Anacelia Gillette <acgillette426@gmail.com>
2017-04-14 14:38:38 -0700

    Votes controller test for making new vote for movie green

--- test/controllers/votes_controller_test.rb
+++ test/controllers/votes_controller_test.rb
@@ -2,12 +2,18 @@ require "test_helper"
 
 describe VotesController do
 
-  it "should create a new vote" do
-
-    proc   {
+  it "should create a new vote for a book" do
+  proc   {
       post create_login_path, { username: "helloWorld" }
       post book_vote_path(contents(:hp).id)
   }.must_change 'User.find(users(:user).id).votes.length', 1
-  end
+end
+
+it "should create a new vote for a movie" do
+  proc   {
+      post create_login_path, { username: "helloWorld" }
+      post movie_vote_path(contents(:guardian).id)
+  }.must_change 'User.find(users(:user).id).votes.length', 1
+end
 
 end
