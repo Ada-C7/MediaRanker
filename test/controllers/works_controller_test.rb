53abb8275b621cfcc9718e4ac0e019c16b37a410
Kelly Souza <kellysouza99@gmail.com>
2017-04-14 15:27:00 -0700

    adds controller test for delete function

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -2,15 +2,21 @@ require "test_helper"
 
 describe WorksController do
 
-    it  "should get index" do
-      get works_path
-      must_respond_with :success
-    end
+  it "should get index" do
+    get works_path
+    must_respond_with :success
+  end
 
   it "should get show page" do
     get work_path(works(:one).id)
     must_respond_with :success
   end
+
+  it "should get category show page" do
+    get works_path("albums")
+    must_respond_with :success
+  end
+
 
 
   it "get edit form should show edit page" do
@@ -47,12 +53,17 @@ describe WorksController do
           description: "Something"
         })
     }.must_change 'Work.count', 1
-
   end
 
   it "should delete a work and redirect to worklist" do
     delete work_path(works(:one).id)
     must_redirect_to category_path("books")
+  end
+
+  it "should affect the model when deleteing a work" do
+    proc {
+      delete work_path(works(:one).id)
+    }.must_change 'Work.count', -1
   end
 
 
