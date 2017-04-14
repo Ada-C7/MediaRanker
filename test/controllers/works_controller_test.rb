52ad07abe4a3fed74d9ac585e3cfc13240598b20
Tamiko Terada <tamikodevo@gmail.com>
2017-04-13 23:57:24 -0700

    test(controller)-add test outline for controller methods

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -26,14 +26,47 @@ describe WorksController do
   describe 'Works#new' do
     it "should get new_book for books" do
       get new_work_path(media_type: "books")
+      must_respond_with :success
     end
 
     it "should get new_book for books" do
       get new_work_path(media_type: "movies")
+      must_respond_with :success
     end
 
     it "should get new_book for books" do
       get new_work_path(media_type: "albums")
+      must_respond_with :success
     end
+  end
+
+  describe 'Works#home' do
+    it "should get new_book for books" do
+      get root_path
+      must_respond_with :success
+    end
+  end
+
+  describe 'Works#create' do
+  end
+
+  describe 'Works#show' do
+    it "should get work path" do
+      get work_path(id: 5)
+      must_respond_with :success
+    end
+  end
+
+  describe 'Works#edit' do
+    it "should get edit_work path" do
+      get edit_work_path(id: 4)
+      must_respond_with :success
+    end
+  end
+
+  describe 'Works#update' do
+  end
+
+  describe 'Works#destroy' do
   end
 end
