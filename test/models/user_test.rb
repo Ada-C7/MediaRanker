f7fb90161b6ebf4c70e66ba734389994b469c9b6
Sofia Kim <sofiamhkim@gmail.com>
2017-04-14 13:06:28 -0700

    validation and test added for user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,11 +1,11 @@
 require "test_helper"
 
-# describe User do
-#   describe 'validations' do
-#     it "Can be created with name" do
-#       a = User.create!(name: "Sofia")
-#       result = user.valid?
-#       result.must_equal true
-#     end
-#   end
-# end
+describe User do
+  describe 'validations' do
+    it "Can be created with name" do
+      a = User.create!(username: "Sofia")
+      result = a.valid?
+      result.must_equal true
+    end
+  end
+end
