f828ac4f8baa2e0978f6e97f68ba41e7a69f8f31
Tamiko Terada <tamikodevo@gmail.com>
2017-04-14 13:21:02 -0700

    name sessions routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,6 @@ class SessionsController < ApplicationController
     else
       render :new
    end
- end
 
   def destroy
     session[:user_id] = nil
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   resources :users
   resources :votes
 
-  get    '/login',   to: 'sessions#new'
+  get    '/login',   to: 'sessions#new', as: 'login'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
 end
