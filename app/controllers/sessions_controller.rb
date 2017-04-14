c875a98e54aa125a2387ca0721decee6f73fc7fe
Jackie Watanabe <sushiigirl@gmail.com>
2017-04-14 11:43:53 -0700

    attempted to show username of logged in user... only shows user id for now

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,18 +2,18 @@ class SessionsController < ApplicationController
   def login_form ; end
 
   def login
-    user = User.find_by_name(params[:name])
+    @user = User.find_by_name(params[:name])
 
     if user
-      session[:user_id] = user.id
-      flash[:success] = "Welcome back, #{user.name}!"
+      session[:user_id] = @user.id
+      flash[:success] = "Welcome back, #{@user.name}!"
       redirect_to root_path
     else
-      user = User.new
-      user.name = params[:name]
-      user.save
-      session[:user_id] = user.id
-      flash[:success] = "#{user.name} added!"
+      @user = User.new
+      @user.name = params[:name]
+      @user.save
+      session[:user_id] = @user.id
+      flash[:success] = "#{@user.name} added!"
       redirect_to root_path
     end
   end
