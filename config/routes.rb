8ebe73403db2b950e1cdb4a248252e334c83bb04
Brenna <bcmdarroch@gmail.com>
2017-04-14 11:56:14 -0700

    user pages working

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
 
   root 'works#index'
+
+  get '/users', to: 'users#index', as: 'users'
+  get '/users/:id', to: 'users#show', as: 'user'
+  
   get '/:category', to: 'works#category_index'
   get '/:category/new', to: 'works#new', as: 'new_work'
 
   resources :works, except: [:new]
 
   post '/:id/upvote', to: 'votes#upvote', as: 'upvote'
-
-  get '/users', to: 'users#index'
-  get '/users/:id', to: 'users#show', as: 'user'
 
   get '/login', to: 'sessions#login_form'
   post '/login', to: 'sessions#login'
