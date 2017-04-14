e13784f1cad31f30d2d4bc6c9ae9d26b939fd3f4
AlisonZ <alisonkzerbe@gmail.com>
2017-04-13 22:17:10 -0700

    links from books index to individual show page. yaaaayayayayay!

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,17 @@ Rails.application.routes.draw do
     root "works#index"
 
     post 'works/:id/upvote', to:'votes#upvote', as: 'upvote'
-    
+
     resources :works
 
     get '/login', to: 'sessions#login_form', as:'login'
     post '/login', to: 'sessions#login'
     delete '/logout', to: 'sessions#logout'
 
-    get '/movies', to: 'movies#index', as: 'movies'
 
     get '/users', to: 'users#index', as: 'users'
     get '/users/:id', to: 'users#show', as: 'user'
+
+    get '/movies', to: 'movies#index', as: 'movies'
+    get '/books', to: 'books#index', as: 'books'
 end
