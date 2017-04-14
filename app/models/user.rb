093d798bc6a94694f841e7cdf1175a7a3b66db1a
Kayla <kubicke.kayla@gmail.com>
2017-04-14 12:44:21 -0700

    Updated relationships so that works and users are connected though votes.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
 
   has_many :votes
+  has_many :works, though: :votes
 
   def index
   end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,6 +1,7 @@
 class Work < ApplicationRecord
 
     has_many :votes
+    has_many :users, though: :votes
 
   def index
   end
