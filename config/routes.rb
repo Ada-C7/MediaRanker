874e78b7cd56eb452e38f82211c4dcd91e17e542
Anacelia Gillette <acgillette426@gmail.com>
2017-04-14 11:29:30 -0700

    Lunch commit. Trying to figure out nested routes for vote

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,13 @@ Rails.application.routes.draw do
   resources :albums
   resources :votes, only: [:create, :update]
 
+  post '/books/:book_id/', to: 'vote#create'
+  post '/movies/:movie_id/', to: 'vote#create'
+  post '/albums/:album_id/', to: 'vote#create'
+
+
+
+
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
