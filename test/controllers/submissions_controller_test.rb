325262ea9b8f701dd7d5eb57dae7ad978e6a880d
janicewilson <janice.wilson@mac.com>
2017-04-14 13:40:29 -0700

    Added tests (finally) for home and submissions controller.

--- test/controllers/submissions_controller_test.rb
+++ test/controllers/submissions_controller_test.rb
@@ -1,46 +1,39 @@
 require "test_helper"
 
 describe SubmissionsController do
-  let(:submission) { submissions :one }
+     let(:submission) { Submission.create!(title: 'Return of the Jedi') }
 
-  it "gets index" do
-    get submissions_url
-    value(response).must_be :success?
-  end
+     it "creates submission" do
+          expect {
+               post submissions_url, params: { submission: { title: 'Return of the Jedi' } }
+          }.must_change "Submission.count"
+     end
 
-  it "gets new" do
-    get new_submission_url
-    value(response).must_be :success?
-  end
+     it "creates submission" do
+          post submissions_url, params: { submission: { title: 'Return of the Jedi', category: 'Album' } }
+          must_redirect_to albums_path
+     end
 
-  it "creates submission" do
-    expect {
-      post submissions_url, params: { submission: {  } }
-    }.must_change "Submission.count"
+     # it "shows submission" do
+     #      get submission_url(submission)
+     #      value(response).must_be :success?
+     # end
 
-    must_redirect_to submission_path(Submission.last)
-  end
-
-  it "shows submission" do
-    get submission_url(submission)
-    value(response).must_be :success?
-  end
-
-  it "gets edit" do
-    get edit_submission_url(submission)
-    value(response).must_be :success?
-  end
-
-  it "updates submission" do
-    patch submission_url(submission), params: { submission: {  } }
-    must_redirect_to submission_path(submission)
-  end
-
-  it "destroys submission" do
-    expect {
-      delete submission_url(submission)
-    }.must_change "Submission.count", -1
-
-    must_redirect_to submissions_path
-  end
+     # it "gets edit" do
+     # get edit_submission_url(submission)
+     # value(response).must_be :success?
+     # end
+     #
+     # it "updates submission" do
+     # patch submission_url(submission), params: { submission: {  } }
+     # must_redirect_to submission_path(submission)
+     # end
+     #
+     # it "destroys submission" do
+     # expect {
+     #  delete submission_url(submission)
+     # }.must_change "Submission.count", -1
+     #
+     # must_redirect_to submissions_path
+     # end
 end
--- test/controllers/votes_controller_test.rb
+++ test/controllers/votes_controller_test.rb
@@ -1,46 +1,46 @@
 require "test_helper"
 
 describe VotesController do
-  let(:vote) { votes :one }
-
-  it "gets index" do
-    get votes_url
-    value(response).must_be :success?
-  end
-
-  it "gets new" do
-    get new_vote_url
-    value(response).must_be :success?
-  end
-
-  it "creates vote" do
-    expect {
-      post votes_url, params: { vote: {  } }
-    }.must_change "Vote.count"
-
-    must_redirect_to vote_path(Vote.last)
-  end
-
-  it "shows vote" do
-    get vote_url(vote)
-    value(response).must_be :success?
-  end
-
-  it "gets edit" do
-    get edit_vote_url(vote)
-    value(response).must_be :success?
-  end
-
-  it "updates vote" do
-    patch vote_url(vote), params: { vote: {  } }
-    must_redirect_to vote_path(vote)
-  end
-
-  it "destroys vote" do
-    expect {
-      delete vote_url(vote)
-    }.must_change "Vote.count", -1
-
-    must_redirect_to votes_path
-  end
+  # let(:vote) { votes :one }
+  #
+  # it "gets index" do
+  #   get votes_url
+  #   value(response).must_be :success?
+  # end
+  #
+  # it "gets new" do
+  #   get new_vote_url
+  #   value(response).must_be :success?
+  # end
+  #
+  # it "creates vote" do
+  #   expect {
+  #     post votes_url, params: { vote: {  } }
+  #   }.must_change "Vote.count"
+  #
+  #   must_redirect_to vote_path(Vote.last)
+  # end
+  #
+  # it "shows vote" do
+  #   get vote_url(vote)
+  #   value(response).must_be :success?
+  # end
+  #
+  # it "gets edit" do
+  #   get edit_vote_url(vote)
+  #   value(response).must_be :success?
+  # end
+  #
+  # it "updates vote" do
+  #   patch vote_url(vote), params: { vote: {  } }
+  #   must_redirect_to vote_path(vote)
+  # end
+  #
+  # it "destroys vote" do
+  #   expect {
+  #     delete vote_url(vote)
+  #   }.must_change "Vote.count", -1
+  #
+  #   must_redirect_to votes_path
+  # end
 end
