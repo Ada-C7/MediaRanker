07d80ef4b2da2df59e21b9e1b18de0ed8176517f
Regan Huff <regan.huff@gmail.com>
2017-04-14 15:33:50 -0700

    Add link and vote count to category list page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "works#index"
 
   resources :works, except: [:new]
-  get "/upvote", to: "works#upvote", as: "upvote"
+  get "/upvote/:id", to: "works#upvote", as: "upvote"
 
   get "/login", to: "sessions#login_form"
   post "/login", to: "sessions#login"
@@ -11,7 +11,5 @@ Rails.application.routes.draw do
 
   get "/:category", to: "works#category_index", as: "category"
   get "/:category/new", to: "works#new", as: "new_work"
-
-
 
 end
