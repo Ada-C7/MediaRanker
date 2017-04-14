800ea5010e2d723d59752b535b400f85afc3c846
Kaitlin Ramirez <rmrz.kt@gmail.com>
2017-04-14 15:16:20 -0700

    failing new test because no method

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -13,7 +13,23 @@ describe WorksController do
     let(:book) {Work.where(category: "book")}
     it "is successful when there are many books" do
       book.count.must_be :>, 0
+      get books_path
+      must_respond_with :success
+    end
+  end
+
+  describe "album_index" do
+    let(:album) {Work.where(category: "album")}
+    it "is successful when there are many books" do
+      album.count.must_be :>, 0
       get albums_path
+      must_respond_with :success
+    end
+  end
+
+  describe "new" do
+    it "runs successfully" do
+      get new_work_path
       must_respond_with :success
     end
   end
