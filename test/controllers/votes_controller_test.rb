f26da438cf89a3cf381338f2dc3cd26dec1a1239
Anacelia Gillette <acgillette426@gmail.com>
2017-04-14 14:54:21 -0700

    Voter controller adding vote to album green

--- test/controllers/votes_controller_test.rb
+++ test/controllers/votes_controller_test.rb
@@ -16,4 +16,12 @@ it "should create a new vote for a movie" do
   }.must_change 'User.find(users(:user).id).votes.length', 1
 end
 
+it "should create a new vote for an album" do
+  proc   {
+      post create_login_path, { username: "helloWorld" }
+      post album_vote_path(contents(:pelican).id)
+  }.must_change 'User.find(users(:user).id).votes.length', 1
+
+end
+
 end
