df9e9b1f6b939a7d0a2cf584482d2b52fed64f01
AlisonZ <alisonkzerbe@gmail.com>
2017-04-13 18:21:01 -0700

    back to adding the votes.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,7 +2,6 @@ class Vote < ApplicationRecord
     belongs_to :work
     belongs_to :user
 
-
     def self.already_created (work_id, user_id)
         Vote.where(user_id: user_id, work_id: work_id).present?
     end
