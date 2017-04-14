185789da4fdc0cc87a1a4f835a1699fe2fac3b13
Olivia <ssecretsharer@gmail.com>
2017-04-13 18:03:53 -0700

    voting logic

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,16 +1,22 @@
 class VotesController < ApplicationController
 
-  def new
-    @vote = Vote.new
-  end
+  def upvote
+    if Vote.already_voted(params[:id], session[:user_id])
+      flash[:error] = "You've already voted for this"
+      redirect_to root_path
+    else
+      vote = Vote.new vote[:work_id] = params[:id]
+      vote[:user_id] = session[:user_id]
 
-  def destroy
-    @vote = Vote.destroy(params[:id])
-    if vote.destroy
-      redirect_to vote_path
+      if vote.save
+        flash[:success] = "You voted, db."
+        redirect_to root_path
+      else
+        flash[:error] = "Can't count that shit"
+        redirect_to login_path
+      end
     end
+
+
   end
-
-
-
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,7 +1,11 @@
 class Vote < ApplicationRecord
-belongs_to :work
-belongs_to :user
-validates :user_id, presence: true, numericality: { only_integer: true, greater_than: 0 }
-validates :work_id, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  belongs_to :work
+  belongs_to :user
+  validates :user_id, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :work_id, presence: true, numericality: { only_integer: true, greater_than: 0 }
 
+  def self.already_voted (work_id, user_id)
+    Vote.where(user_id: user_id, work_id: work_id).present?
+
+  end
 end
