623e57136189037cfb9e5af8bfc135a2049085a0
Anacelia Gillette <acgillette426@gmail.com>
2017-04-14 09:53:30 -0700

    Made login page, made session controller methods

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,2 +1,23 @@
 class SessionController < ApplicationController
+
+  def login
+    user = User.find_by_username(params[:username])
+
+    if user
+      session[:user_id] = user.id
+      flash[:success] = "Welcome back #{user.username}"
+      redirect_to root_path
+    else
+      new_user = User.new
+      user.username = params[:username]
+      flash[:success] = "Welcome #{user.username}"
+      redirect_to root_path
+    end
+  end
+
+  def logout
+    session.delete[:user_id]
+    flash[:success] = "You are successfully logged out"
+    redirect_to root_path
+
 end
