83633b5838e97cd152286399deaf801a92178e10
Bo Trethewey <botrethewey@gmail.com>
2017-04-14 11:17:31 -0700

    updated user controller test and added action methods to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @result_user = User.find_by_id(params[:id])
+    if !@result_user
+      render_404
+    end
+  end
+
+  def create
+    @work = Work.create(work_params)
+  end
+
+  def work_params
+    params.require(:user).permit(:name)
+  end
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,29 @@
 require "test_helper"
 
 describe User do
-  let(:user) { User.new }
+  describe "validations" do
+    it "is invalid without a name" do
+      user = User.new
+      user.valid?.must_equal false
+      user.errors.messages.must_include :name
+    end
 
-  it "must be valid" do
-    value(user).must_be :valid?
+    it "is valid with a name" do
+      users(:user).valid?.must_equal true
+    end
   end
+
+  describe "Entity Relationship" do
+    it "can access Vote objects" do
+      user = User.create(name: "ken")
+      work = works(:lee)
+      vote = Vote.create(user_id: user.id, work_id: work.id)
+      work_two = works(:mee)
+      vote_two= Vote.create(user_id: user.id, work_id: work_two.id)
+
+      user.votes[0].class.must_equal Vote
+      user.votes.size.must_equal 2
+    end
+  end
+
 end
