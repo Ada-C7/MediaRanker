95cab35b9249788ee1a277b7e92dea5bc73ef881
Alix Hamilton <alix.hamilton@gmail.com>
2017-04-14 14:17:19 -0700

    adds user model tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,24 @@
 require "test_helper"
 
 describe User do
-  let(:user) { User.new }
 
-  it "must be valid" do
-    value(user).must_be :valid?
+  it "doesn't create a user without a user name" do
+    value(User.new).wont_be :valid?
+  end
+
+  it "creates a valid user given a user name" do
+    users = users(:alixhami)
+
+    value(users).must_be :valid?
+  end
+
+  it "must have a unique username" do
+    user = users(:alixhami)
+    user.save.must_equal true
+
+    duplicate_user = User.new(username: user.username)
+
+    duplicate_user.save.must_equal false
+    duplicate_user.errors.messages[:username].must_equal ["has already been taken"]
   end
 end
