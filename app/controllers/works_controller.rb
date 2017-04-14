7b0ef0f95cea6decd5fb7185b3fe24beeecaefaa
amb54 <ambarklund@hotmail.com>
2017-04-14 14:28:33 -0700

    Updated index for works controller to ordered records. Subsequently changed the code in index.

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,13 +1,19 @@
 class WorksController < ApplicationController
   def index
-    @albums = Work.where(category: "album").limit(10)
-    @books = Work.where(category: "book").limit(10)
-    @movies = Work.where(category: "movie").limit(10)
+
+    @works = Work.left_joins(:votes).group(:id).order('COUNT(votes.id) DESC')
+    @albums = @works.where(category: "album").limit(10)
+    @books = @works.where(category: "book").limit(10)
+    @movies = @works.where(category: "movie").limit(10)
+    # @albums = Work.where(category: "album").limit(10)
+    # @books = Work.where(category: "book").limit(10)
+    # @movies = Work.where(category: "movie").limit(10)
     # @albums = Work.ordered_works("album")[0...10]
     # @books = Work.ordered_works("book")[0...10]
     # @movies = Work.ordered_works("movie")[0...10]
     # leader_id = Work.order_by_votes(Work.all)[0]
     # @leader = Work.find_by(id: leader_id)
+
 
   end
 
@@ -77,7 +83,7 @@ class WorksController < ApplicationController
       path = books_path
     elsif session[:category] == "movie"
       path = movies_path
-    end    
+    end
     redirect_to path
 
   end
