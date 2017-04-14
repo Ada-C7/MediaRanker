c03bc49786d44b40385770aff2e7ef6ed05ef87f
Kaitlin Ramirez <rmrz.kt@gmail.com>
2017-04-14 00:50:15 -0700

    user log in all fixed. shows username. logic was wrong

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,25 +1,24 @@
 class SessionsController < ApplicationController
-  def login_form
-    @user = User.new
-  end
+  def login_form; end
 
   def login
-    @user = User.new
-    if @user.save
-      session[:user_id] = @user.id
-      flash[:success] = "Successfully created new user #{@user.username} with ID #{@user.id}"
+    user = User.find_by(username: params[:username])
+    if user
+      session[:user_id] = user.id
+      flash[:success] = "Successfully logged in as existing user #{user.username}"
       redirect_to mainpages_path
-    elsif @user = User.find_by_name(params[:username])
-      session[:user_id] = @user.id
-      flash[:success] = "Successfully logged in as existing user #{@user.username}"
+    elsif
+      user = User.create(username: params[:username])
+      session[:user_id] = user.id
+      flash[:success] = "Successfully created new user #{user.username} with ID #{user.id}"
       redirect_to mainpages_path
     else
       if user.errors.any?
         user.errors.each do |column, message|
           flash.now[:error] = "Could not log in #{message}"
         end
+        render :login_form
       end
-      render :login_form
     end
   end
 
