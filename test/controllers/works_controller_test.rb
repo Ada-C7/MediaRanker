2f2b57fa64355f09638c991312811e233408bcf5
Kaitlin Ramirez <rmrz.kt@gmail.com>
2017-04-14 15:05:51 -0700

    some works controllers tests and work yaml made - thanks Anna :)

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -1,14 +1,26 @@
 require "test_helper"
 
 describe WorksController do
-  describe "album_index" do
-    it "responds successfully" do
+  describe "index" do
+    it "is successful when there are many works" do
+      Work.count.must_be :>, 0
+      get albums_path
+      must_respond_with :success
+    end
+  end
 
+  describe "book_index" do
+    let(:book) {Work.where(category: "book")}
+    it "is successful when there are many books" do
+      book.count.must_be :>, 0
+      get albums_path
+      must_respond_with :success
     end
   end
 
   describe "show" do
     it "shows a work that exists" do
+      skip
       work = Work.first
       get work_path(work)
       must_respond_with :success
