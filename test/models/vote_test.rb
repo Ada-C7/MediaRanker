e92a4bb2355e75da2c017d5ddc6be88ef959aed0
Alix Hamilton <alix.hamilton@gmail.com>
2017-04-14 14:10:53 -0700

    vote model tests

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -2,30 +2,28 @@ require "test_helper"
 
 describe Vote do
 
-  describe 'validations' do
-    it 'is invalid without parameters' do
-      value(Vote.new).wont_be :valid?
-    end
+  it 'is invalid without parameters' do
+    value(Vote.new).wont_be :valid?
+  end
 
-    it 'is invalid without a work ID' do
-      vote = Vote.new(user_id: 1)
+  it 'is invalid without a work ID' do
+    vote = Vote.new(user_id: 1)
 
-      value(vote).wont_be :valid?
-      vote.errors.messages.must_include :work_id
-    end
+    value(vote).wont_be :valid?
+    vote.errors.messages.must_include :work_id
+  end
 
-    it 'is invalid without a user ID' do
-      vote = Vote.new(work_id: 1)
+  it 'is invalid without a user ID' do
+    vote = Vote.new(work_id: 1)
 
-      value(vote).wont_be :valid?
-      vote.errors.messages.must_include :user_id
-    end
+    value(vote).wont_be :valid?
+    vote.errors.messages.must_include :user_id
+  end
 
-    it "must be valid when given appropriate parameters" do
-      vote = votes(:one)
+  it "must be valid when given appropriate parameters" do
+    vote = votes(:one)
 
-      vote.save.must_equal true
-    end
+    vote.save.must_equal true
   end
 
 end
