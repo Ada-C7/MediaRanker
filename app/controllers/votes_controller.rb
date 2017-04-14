3ef6d76d44f71db707d827dc1cd77f61c93ad5e5
sai samant <saisamant@gmail.com>
2017-04-14 08:46:22 -0700

    votes WIP

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,24 +1,27 @@
 class VotesController < ApplicationController
 
   def create
-    vote = Vote.new
-    vote.work_id = params[:id]
-    if session[:user_id]
-      vote.user_id = session[:user_id]
-    else
+    # binding.pry
+    if !session[:user_id]
       flash[:error] = "You must log in to do that"
-      redirect_to :back
+    elsif
+      @vote = Vote.new
+      @vote.work_id = params[:id]
+      @vote.user_id = session[:user_id]
+      flash[:success] = "Work id: #{@vote.work_id} and User ID: #{@vote.user_id} for vote"
+      if @vote.save
+        flash[:success] = "Successfully Voted"
+      else
+        flash[:error] = "Could not upvote"
+        @vote.errors.each do |field, message|
+          flash[:error] = "user #{message}"
+        end
+      end
     end
 
-    if Vote.where({user_id: session[:user_id], work_id: params[:id]})
-      flash[:error] = "Could not upvote"
-      redirect_to :back
-    end
 
-    if work.save
-      flash[:success] = "Successfully Voted"
-      redirect_to :back
-    end
+    redirect_to :back
+
   end
 
 end
