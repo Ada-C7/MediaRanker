dcc1d3e969cbcd21ae0c0f17ea878d60b0e1285e
Cara Comfort <cara.comfort10@gmail.com>
2017-04-13 18:49:22 -0700

    Implement user index route, controller method, and view

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,6 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+
+  get 'users', to: 'users#index', as: 'users'
 end
