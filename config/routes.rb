6e3493bb82a8e78205f7e5e193df7c91f74f3065
Bo Trethewey <botrethewey@gmail.com>
2017-04-14 10:37:29 -0700

    Update routes and indexview for each media category

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'homes#index'
-  post '/works/:id/upvote', to: "votes#create", as: 'upvote'
 
-  resources :works, except: [:new]
+  resources :works, except: [:index, :new]
+
   resources :movies, except: [:destroy, :create, :show, :edit, :update]
   resources :albums, except: [:destroy, :create, :show, :edit, :update]
   resources :books, except: [:destroy, :create, :show, :edit, :update]
