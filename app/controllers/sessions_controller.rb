c66467af11a30ea1bc0c37a650d3bdd199d8e71e
Kaitlin Ramirez <rmrz.kt@gmail.com>
2017-04-13 23:24:27 -0700

    user login almost working, something wrong with name display

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,37 @@
 class SessionsController < ApplicationController
   def login_form; end
 
-  def login
-    raise
+  def new
+    @user = User.new
   end
+
+  def login
+    @user = User.new
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:success] = "Successfully created new user #{@user.username} with ID #{@user.id}"
+      redirect_to mainpages_path
+    elsif @user = User.find_by_name(params[:name])
+      session[:user_id] = @user.id
+      flash[:success] = "Successfully logged in as existing user #{@user.username}"
+      redirect_to mainpages_path
+    else
+      if user.errors.any?
+        user.errors.each do |column, message|
+          flash.now[:error] = "Could not log in #{message}"
+        end
+      end
+      render :login_form
+    end
+  end
+
+  def logout
+    session[:user_id] = nil
+    flash[:success] = "Successfully logged out."
+  end
+
+    private
+    def user_params
+      return params.require(:user).permit(:username)
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,24 +6,4 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-
-  #Create a user
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.create(user_params)
-    if @user.id != nil
-      flash[:success] = "Successfully created new user #{user.username} with ID #{user.id}"
-      redirect_to mainpages_path
-    else
-      render :new
-    end
-  end
-
-  private
-  def user_params
-    return params.require(:user).permit(:username)
-  end
 end
