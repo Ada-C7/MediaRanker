2b0eeb1ab54e59a74bd3555f39e8173fae0b5bb3
Maria McGrew <mcg.maria@gmail.com>
2017-04-13 21:28:44 -0700

    books and albums show page done

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,4 +7,22 @@ class BooksController < ApplicationController
     @work = Work.find(params[:id])
   end
 
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work = Work.new(books_params)
+    if @work.save
+      redirect_to books_path
+    else
+      render :new
+    end
+  end
+
+private
+  def book_params
+    return params.require(:work).permit(:title, :creator, :publication_year, :description)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'works#index', as: 'works'
 
-  resources :works, only:[:index, :show]
-  resources :books, only:[:index, :show]
+  resources :works, only:[:index, :show, :new, :create]
+  resources :books, only:[:index, :show, :new, :create]
+  resources :albums, only:[:index, :show]
+
 
 end
