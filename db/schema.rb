15b3cd3118aa3114c9ae24a29610b0b70417031b
Kayla <kubicke.kayla@gmail.com>
2017-04-14 15:44:23 -0700

    Attempting to debug heroku deployment.

--- db/schema.rb
+++ db/schema.rb
@@ -9,6 +9,9 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
+User.destroy_all
+Vote.destroy_all
+Work.destroy_all
 
 ActiveRecord::Schema.define(version: 20170410232754) do
 
