17082a9e928b05a8d0f47eef4cde6d2772b34e64
Marisol Lopez <lmarisolopez@gmail.com>
2017-04-13 17:09:31 -0700

    added login and logout feature

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,5 +1,6 @@
 class Work < ApplicationRecord
   has_many :users, through: :votes
+
   validates :title, presence: true
   validates :category, presence: true, inclusion: { in: %w(movie book album)}
 
@@ -7,5 +8,5 @@ class Work < ApplicationRecord
     category = category.singularize.downcase
     Work.where(category:category)
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   root 'welcome_page#index'
   resources :works, except: :index
   get '/:category', to: 'works#index', constraints: { category: /(books)|(movies)|(albums)/}, as: "category"
-  end
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+
+end
