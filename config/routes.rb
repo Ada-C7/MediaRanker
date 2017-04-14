e8b061476a9d8172386a77bebdd4408e8afa70c6
Andrea Valliere <amvalliere@gmail.com>
2017-04-13 17:14:30 -0700

    Added/edited routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,29 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
 
-  get 'movies/index'
+  root to: 'welcome#index'
 
-  get 'movies/new'
+  resources :works, except: :index
+  get '/:category', to: 'works#index', constraints: { category: /(books)|(movies)|(albums)/}, as: "category"
 
-  get 'movies/:id', to: 'movies#show', as: 'movie'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
 
-  root to: 'works#index'
-  get 'votes/index'
+  delete 'logout', to: 'sessions#destroy'  
 
-  get 'users/index'
-
-  get 'works/index'
+  # get 'movies/index'
+  #
+  #
+  # get 'movies/new', to: 'movies#new', as: 'new_movie'
+  # post 'movies', to: 'movies#create'
+  # get 'movies/:id', to: 'movies#show', as: 'movie'
+  #
+  #
+  # get 'votes/index'
+  #
+  # get 'users/index'
+  #
+  # get 'works/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
