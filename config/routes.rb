2ffbe17f72d19e265e6c1daa03dbb4d3acf3a8ae
Tehut Getahun <tehut.m.g@gmail.com>
2017-04-14 14:58:24 -0700

    removed welcome controller and made works#index root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root "welcome#index", as:'welcome'
+  root "works#index", as:'welcome'
   resources :welcome #look up how to use "only get" here. Have unnecceary routes
   #i think i can just write it the "handroll" way but dont want to risk breaking
   #things just yet
