069d947f6daa6ec330e4ec5863c5ae68574e3fe8
Bo Trethewey <botrethewey@gmail.com>
2017-04-15 16:49:12 -0700

    added votes controller test and updated test fixtures

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,12 +1,44 @@
 class VotesController < ApplicationController
-
   def create
     user_name
-    @vote = Vote.new
-    @vote.work_id = params[:vote[:work_id]]
-    @vote.user_id = params[:vote[:user_id]]
-    #session conditional to focre login to vote
-     flash[:sucess] = Successfully upvoted!
+    if !@user
+      flash[:failure] = "You must log in to do that"
+    else
+      @vote = Vote.new
+      @vote.work_id = params[:work_id]
+      @vote.user_id = @user.id
+      if @vote.save
+        flash[:success] = "Successfully upvoted!"
+      else
+        flash[:failure] = "Could not upvote"
+        flash[:error] = "user: has already voted for this work"
+      end
+    end
+    redirect_to work_path(params[:work_id])
   end
 
+  def create_from_list
+    user_name
+    if !@user
+      flash[:failure] = "You must log in to do that"
+    else
+      @vote = Vote.new
+      @vote.work_id = params[:work_id]
+      @vote.user_id = @user.id
+      if @vote.save
+        flash[:success] = "Successfully upvoted!"
+      else
+        flash[:failure] = "Could not upvote"
+        flash[:error] = "user: has already voted for this work"
+      end
+    end
+    work = Work.find_by_id(params[:work_id])
+    if work.category == "movie"
+      redirect_to movies_path
+    elsif work.category == "book"
+      redirect_to books_path
+    else
+      redirect_to albums_path
+    end
+  end
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,7 @@
 class Vote < ApplicationRecord
-  belongs_to :work
+  belongs_to :work,  counter_cache: true
   belongs_to :user
+
+  validates :user_id, uniqueness: { scope: :work_id,
+    message: "user: has already voted for this work" }
 end
