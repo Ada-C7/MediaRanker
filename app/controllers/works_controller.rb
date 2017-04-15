f1d9b3f81dfca9195369ae707a32e9d7fd22593f
Sofia Kim <sofiamhkim@gmail.com>
2017-04-15 09:35:30 -0700

    create method under works controller deleted

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -4,17 +4,17 @@ class WorksController < ApplicationController
     @work=Work.find(params[:id])
   end
 
-  def create
-    work = Work.new
-    work.category = "movie"
-    work.user_id = session[:user_id]
-    work.title = work_params[:title]
-    work.publication_year = work_params[:publication_year]
-    work.description = work_params[:description]
-
-    work.save!
-    redirect_to movies_path
-  end
+  # def create
+  #   work = Work.new
+  #   work.category = "movie"
+  #   work.user_id = session[:user_id]
+  #   work.title = work_params[:title]
+  #   work.publication_year = work_params[:publication_year]
+  #   work.description = work_params[:description]
+  #
+  #   work.save!
+  #   redirect_to movies_path
+  # end
 
   #
   # def edit
