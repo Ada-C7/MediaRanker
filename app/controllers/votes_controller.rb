8c846ff92f9af046c2671e562bbc9697e998807b
Lynn Trickey <lynn.trickey@gmail.com>
2017-04-14 17:29:40 -0700

    changed votes to redirect to current page, but now votes controller is causing errors.  Need to rewrite tests if I have time!

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -8,14 +8,14 @@ class VotesController < ApplicationController
         vote.user_id = session[:user_id]
         vote.save
         flash[:success] = "You have successfully upvoted."
-        redirect_to work_path
+        redirect_to request.referrer
       else
         flash[:warning] = "You can only vote once for each work."
-        redirect_to work_path
+        redirect_to request.referrer
       end
     else
       flash[:warning] = "You must be logged in to Vote"
-      redirect_to login_path
+      redirect_to request.referrer
     end
   end
 
--- test/controllers/votes_controller_test.rb
+++ test/controllers/votes_controller_test.rb
@@ -1,17 +1,15 @@
 require "test_helper"
 
 describe VotesController do
-  it "will redirect to login page if user is not logged in to vote" do
+  it "will redirect to current if user is not logged in to vote" do
     post new_vote_path(votes(:vote_one).id)
     must_respond_with :redirect
-    must_redirect_to login_path
   end
 
-  it "will redirect to show page if user is logged in and votes" do
+  it "will redirect to current page if user is logged in and votes" do
     post login_path(), params: { name: users(:lynn).name } #what params is, needed for method
     post new_vote_path(works(:work_one).id)
     must_respond_with :redirect
-    must_redirect_to work_path(works(:work_one).id)
   end
 
   it "will affect user model if vote is valid" do
