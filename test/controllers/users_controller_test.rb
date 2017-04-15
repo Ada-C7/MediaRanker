7d078a00a9077e0b6a799162eb92ecee8ebe1459
add2point71dots <alcland@gmail.com>
2017-04-14 22:01:05 -0700

    wrote users controller tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 describe UsersController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  it "should get index" do
+    get users_path
+    must_respond_with :success
+  end
+
+  it "should show individual user" do
+    user = User.create(username: "New User")
+    get user_path(user.id)
+    must_respond_with :success
+  end
+
+  it "should redirect to 404 if given bad user id" do
+    get user_path(1)
+    must_respond_with :missing
+  end
 end
