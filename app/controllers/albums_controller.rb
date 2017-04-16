2974c30feae7eafc3b0cbddd6847cf1f62331f90
Sofia Kim <sofiamhkim@gmail.com>
2017-04-15 23:05:45 -0700

    create method redirect to login path added under albums_controller

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -8,6 +8,9 @@ class AlbumsController < ApplicationController
   end
 
   def create
+    if session[:user_id].nil?
+      return redirect_to login_path
+    end
     work = Work.new
     work.category = "album"
     work.user_id = session[:user_id]
