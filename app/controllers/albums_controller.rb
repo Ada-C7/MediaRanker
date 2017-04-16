28a708d8ca9b039453ec27294671dc591f58ed02
Bo Trethewey <botrethewey@gmail.com>
2017-04-15 17:46:31 -0700

    add html and foundation css to albums view files, and update albums controller

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -2,7 +2,7 @@ class AlbumsController < WorksController
 
   def index
     user_name
-    @albums = Work.where(category: "album")
+    @albums = Work.where(category: "album").order(votes_count: :desc)
   end
 
   def new
