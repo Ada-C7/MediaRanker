98ac0c649566f9bb507c79c8bc299bde18f93178
Kelly Souza <kellysouza99@gmail.com>
2017-04-16 12:53:26 -0700

    Puts upvote back where it belongs

--- app/models/work.rb
+++ app/models/work.rb
@@ -11,22 +11,6 @@ class Work < ApplicationRecord
     #   end
     #   return @categories_list
     # end
-    def upvote
-      if !session[:user_id]
-        flash[:error] = "You must log in to do that"
-        redirect_to :back
-      elsif Vote.where(user_id: session[:user_id]).where(work_id: params[:id]) == []
-        work = Work.find(params[:id])
-        vote = Vote.create
-        vote.work_id = work.id
-        vote.user_id = session[:user_id]
-        vote.save
-        flash[:success] = "Successfully upvoted!"
-        redirect_to :back
-      else
-        flash[:error] = "Could not upvote"
-        redirect_to :back
-      end
-    end
+    
 
 end
