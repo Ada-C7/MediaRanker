919dc7c35aabdb91e3fcddc5322bf9545aa9f648
Maria McGrew <mcg.maria@gmail.com>
2017-04-16 10:36:08 -0700

    added columns to books model and passed first books model test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170414033821) do
+ActiveRecord::Schema.define(version: 20170416172929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,8 +21,13 @@ ActiveRecord::Schema.define(version: 20170414033821) do
   end
 
   create_table "books", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.string   "category"
+    t.string   "title"
+    t.string   "creator"
+    t.string   "publication_year"
+    t.string   "description"
   end
 
   create_table "works", force: :cascade do |t|
--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,1 +1,11 @@
 require "test_helper"
+
+describe Book do
+  describe 'validations' do
+    it 'requires a title' do
+      book = Book.new
+      result = book.valid?
+      result.must_equal false
+    end
+  end
+end
