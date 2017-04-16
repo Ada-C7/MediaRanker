9ff505c7e0f15bf9a3f2115dc442ed4e4b083870
Bo Trethewey <botrethewey@gmail.com>
2017-04-15 17:36:34 -0700

    added session tests for model and controller, and updated flash error message placement

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,18 +9,22 @@ class SessionsController < ApplicationController
       flash[:success] = "Successfully logged in as existing user #{ user.name }"
       redirect_to root_path
     else
-      user = User.new
-      user.name = params[:name]
-      user.save
-      session[:user_id] = user.id
-      flash[:success] = "Successfully created new user #{ user.name } with ID #{ user.id }"
-      redirect_to root_path
+      user = User.create(name: params[:name])
+      if user.id != nil
+        session[:user_id] = user.id
+        flash[:success] = "Successfully created new user #{user.name} with ID #{user.id}"
+        redirect_to root_path
+      else
+        flash.now[:failure] = "Could not log in"
+        flash.now[:error] = "username: can't be blank"
+        render "login_form"
+      end
     end
   end
 
   def logout
-   session.delete(:user_id)
-   flash[:success] = "Successfully logged out"
-   redirect_to root_path
+    session.delete(:user_id)
+    flash[:success] = "Successfully logged out"
+    redirect_to root_path
   end
 end
