21afd14bdf865179c51872e5930c3182e002e5b9
Sofia Kim <sofiamhkim@gmail.com>
2017-04-16 00:56:54 -0700

    destroy action added under work controller

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -3,23 +3,10 @@ class WorksController < ApplicationController
   def show
     @work=Work.find(params[:id])
   end
-
-  # def create
-  #   work = Work.new
-  #   work.category = "movie"
-  #   work.user_id = session[:user_id]
-  #   work.title = work_params[:title]
-  #   work.publication_year = work_params[:publication_year]
-  #   work.description = work_params[:description]
-  #
-  #   work.save!
-  #   redirect_to movies_path
-  # end
-
-  #
-  # def edit
-  #   @work=Work.find(params[:id])
-  # end
+  
+  def edit
+    @work=Work.find(params[:id])
+  end
 
   def update
     @work=Work.find(params[:id])
@@ -28,10 +15,10 @@ class WorksController < ApplicationController
     end
   end
 
-  # def destroy
-  #   Work.destroy(params[:id])
-  #   redirect_to works_path
-  # end
+  def destroy
+    Work.destroy(params[:id])
+    redirect_to root_path
+  end
 
   private
 
