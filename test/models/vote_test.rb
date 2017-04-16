225c95d17796862e8d4ef4fdff02ea73123d5fb5
Regan Huff <regan.huff@gmail.com>
2017-04-16 14:57:02 -0700

    Add tests for vote, presence of work_id and user_id

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -2,8 +2,27 @@ require "test_helper"
 
 describe Vote do
   let(:vote) { Vote.new }
+  let(:user) { users(:betty) }
+  let(:movie) { works(:anything) }
+  let(:book) { works(:elemental) }
+  let(:bettyvote) {votes(:betty_bookvote)}
 
-  it "must be valid" do
-    value(vote).must_be :valid?
+  it "cannot create a vote without a work_id and a user_id" do
+    vote.save
+    vote.valid?.must_equal false
+  end
+
+  it "can create a vote with both a work_id and a user_id" do
+    vote.user = user
+    vote.work = movie
+    vote.save
+    vote.valid?.must_equal true
+  end
+
+  it "cannot create a second vote from the same user for the same work" do
+    vote.user = user
+    vote.work = book
+    vote.save
+    vote.valid?.must_equal false
   end
 end
