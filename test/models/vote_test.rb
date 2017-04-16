8b03869782d07da3bfdc14b6df1126edada6f817
Bo Trethewey <botrethewey@gmail.com>
2017-04-15 17:13:56 -0700

    added vote model tests

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,9 +1,30 @@
 require "test_helper"
 
 describe Vote do
-  let(:vote) { Vote.new }
+  describe "validations" do
+    it "is valid with a user and a work" do
+      votes(:one).valid?.must_equal true
+    end
 
-  it "must be valid" do
-    value(vote).must_be :valid?
+    it "is invalid without a user" do
+      vote = votes(:three)
+      vote.valid?.must_equal false
+      vote.errors.messages.must_include :user
+    end
+
+    it "is invlaid without a work" do
+      vote = votes(:two)
+      vote.valid?.must_equal false
+      vote.errors.messages.must_include :work
+    end
   end
+
+  describe "Entity Relationship" do
+    it "can access User objects" do
+      vote = votes(:one)
+      users(:user).must_equal vote.user
+      works(:lee).must_equal vote.work
+    end
+  end
+
 end
