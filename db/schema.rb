d13c05d2a3bf72ec54fdf743faf5dc0da854506a
laura <lmelgarejos@unal.edu.co>
2017-04-17 14:47:10 -0700

    Created migration to update user's model

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170413210842) do
+ActiveRecord::Schema.define(version: 20170417213657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.string   "oauth_uid"
+    t.string   "email"
+    t.string   "oauth_provider"
   end
 
   create_table "votes", force: :cascade do |t|
