b439cd327fb213c93ce51b71b602d8c3ebd785f4
Tamiko Terada <tamikodevo@gmail.com>
2017-04-17 02:08:26 -0700

    add links to the bottom of top 10

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # welcome page shows top 10 of all three works
   root 'works#home'
-  get '/:media_type', to: 'works#index', as: 'works', constraints: { media_type: /(albums)|(movies)|(books)/ }
+  get '/:media_type', to: 'works#index', as: "works", constraints: { media_type: /(albums)|(movies)|(books)/ }
   get '/:media_type/new', to: 'works#new', as: 'new_work', constraints: { media_type: /(albums)|(movies)|(books)/ }
   post '/works', to: 'works#create'
   get '/works/:id', to: 'works#show', as: 'work'
