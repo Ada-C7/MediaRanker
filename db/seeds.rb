94b2ffc01c472d0bd2bbc510309dcee83c83c0ed
EricaJCase <ericajcase@gmail.com>
2017-04-16 23:30:21 -0700

    seeds.rb added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'media_seeds.csv'))
+
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  w = Work.new
+  w.title = row['title']
+  w.category = row['category']
+  w.created_by = row['creator']
+  w.description = row['description']
+  w.published = row['publication_year']
+  w.save
+
+puts "Added: #{w.title} by #{w.created_by}!"
+end
+
+puts "There are now #{Work.count} rows in the table."
