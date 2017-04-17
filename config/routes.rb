ef36a7f227abf81c89e834067490c2c6c3ece6dd
Olivia <ssecretsharer@gmail.com>
2017-04-17 16:47:18 -0700

    fixed vote validate issue,...but just comment it tf out

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,9 @@ Rails.application.routes.draw do
   post "/login", to: "sessions#login"#, as: "login"
   delete "/login", to: "sessions#logout"
 
-  category_constraints = { category: /(albums)|(books)|(movies)/}
-  get '/:category', to: 'works#index', as: 'works', constraints: category_constraints
-  get '/:category/new', to: 'works#new', as: 'new_work', constraints: category_constraints
-  post '/:category', to: 'works#create', constraints: category_constraints
+  # category_constraints = { category: /(albums)|(books)|(movies)/}
+  # get '/:category', to: 'works#index', as: 'works', constraints: category_constraints
+  # get '/:category/new', to: 'works#new', as: 'new_work', constraints: category_constraints
+  # post '/:category', to: 'works#create', constraints: category_constraints
 
 end
