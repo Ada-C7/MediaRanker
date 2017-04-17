d26b2f3fd0473597fbef46c91a919a71f6b1c965
Maria McGrew <mcg.maria@gmail.com>
2017-04-16 23:52:04 -0700

    completed works controller tests

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -1,7 +1,33 @@
 require "test_helper"
 
 describe WorksController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "index" do
+    it "Responds successfully" do
+      Work.count.must_be :>, 0
+      get works_path
+      must_respond_with :success
+    end
+
+    it "still responds successfully when there are no works" do
+      Work.destroy_all
+      get works_path
+      must_respond_with :success
+    end
+  end
+
+  describe "show" do
+    it "shows a work that exists" do
+      work = Work.first
+      get work_path(work)
+      must_respond_with :success
+    end
+
+    it "return a 404 if the work doesn't exist" do
+      work_id = Work.last.id
+      work_id += 1
+      get work_path(work_id)
+
+      must_respond_with :not_found
+    end
+  end
 end
