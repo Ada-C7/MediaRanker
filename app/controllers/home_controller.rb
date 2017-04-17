61fe935acbbbc6b5e168c09c021ee8e45458c9d4
Cara Comfort <cara.comfort10@gmail.com>
2017-04-17 00:02:02 -0700

    Update homepage to include spotlight and list work by descending votes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,10 @@
 class HomeController < ApplicationController
   def index
     @works = {}
-    @works["movies"] = Work.where(category: "movie").limit(10)
-    @works["books"] = Work.where(category: "book").limit(10)
-    @works["albums"] = Work.where(category: "album").limit(10)
-    # @movies = Work.where(category: "movie").limit(10)
-    # @books = Work.where(category: "book").limit(10)
-    # @albums = Work.where(category: "album").limit(10)
+    @works["movies"] = Work.top_ten("movie")
+    @works["books"] = Work.top_ten("book")
+    @works["albums"] = Work.top_ten("album")
 
-    # @albums = Work.where(category: album).order(votes: :desc).limit(10)
-    # Book.where(author: "Roxane Gay").order(title: :desc)
+    @spotlight = Work.spotlight
   end
 end
