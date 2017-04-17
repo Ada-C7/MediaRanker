aeff3832a4d58684044c0ed2a82714c0402d89d6
Olivia <ssecretsharer@gmail.com>
2017-04-17 16:03:18 -0700

    maybe this will fix the category

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
   root "works#index"
 
-post 'works/:id/upvote', to:'votes#upvote', as: 'upvote'
-resources :works
-resources :users
+  resources :works
+  resources :users
 
+  post 'works/:id/upvote', to:'votes#upvote', as: 'upvote'
 
-get "/login", to: "sessions#login_form"#, as: "login_form"
-post "/login", to: "sessions#login"#, as: "login"
+  get "/login", to: "sessions#login_form"#, as: "login_form"
+  post "/login", to: "sessions#login"#, as: "login"
+  delete "/login", to: "sessions#logout"
 
-delete "/login", to: "sessions#logout"
+  category_constraints = { category: /(albums)|(books)|(movies)/}
+  get '/:category', to: 'works#index', as: 'works', constraints: category_constraints
+  get '/:category/new', to: 'works#new', as: 'new_work', constraints: category_constraints
+  post '/:category', to: 'works#create', constraints: category_constraints
 
 end
