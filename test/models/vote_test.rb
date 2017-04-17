85831e4d828df41bf478cafed1c0cf5da60c8e95
Regan Huff <regan.huff@gmail.com>
2017-04-16 17:05:14 -0700

    Add tests for vote model

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -3,9 +3,30 @@ require "test_helper"
 describe Vote do
   let(:vote) { Vote.new }
   let(:user) { users(:betty) }
+  let(:fred) { users(:fred) }
   let(:movie) { works(:anything) }
   let(:book) { works(:elemental) }
   let(:bettyvote) {votes(:betty_bookvote)}
+
+  it "can set the user through 'user'" do
+    bettyvote.user = fred
+    bettyvote.user_id.must_equal fred.id
+  end
+
+  it "can set the user through 'user_id'" do
+    bettyvote.user_id = fred.id
+    bettyvote.user.must_equal fred
+  end
+
+  it "can set the work through 'work'" do
+    bettyvote.work = movie
+    bettyvote.work_id.must_equal movie.id
+  end
+
+  it "can set the work through 'work_id'" do
+    bettyvote.work_id = movie.id
+    bettyvote.work.must_equal movie
+  end
 
   it "cannot create a vote without a work_id and a user_id" do
     vote.save
@@ -25,4 +46,6 @@ describe Vote do
     vote.save
     vote.valid?.must_equal false
   end
+
+
 end
