fc07edd71d4e1aaaf5a31e6175fff21c5fe36b30
Allison Northrop <agnorthrop@gmail.com>
2017-04-17 22:34:33 -0700

    added method to votes controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,3 +1,25 @@
 class VotesController < ApplicationController
-  # TODO again, trying to figure out if I need this 
+
+  def index
+    @votes = Vote.all
+  end
+
+  def show
+    @vote = Vote.find(params[:id])
+  end
+
+  def new
+    @vote = Vote.new
+  end
+
+  def create
+    @vote = Vote.new
+    @vote.save
+  end
+
+  def destroy
+    Vote.destroy(params[:id])
+  end
+
+
 end
