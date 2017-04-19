adfb62977da6130fe99090b1787d4525f10d4893
Janice Lichtman <jalichtman@gmail.com>
2017-04-18 23:45:20 -0700

    items#index passes tests

--- test/controllers/items_controller_test.rb
+++ test/controllers/items_controller_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 describe ItemsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+
+    describe "index" do
+      it "is successful when there are many items" do
+        Item.count.must_be :>, 0
+        get items_path
+        must_respond_with :success
+      end
+
+      it "is successful when there are zero items" do
+        Item.destroy_all
+        get items_path
+        must_respond_with :success
+      end
+    end
+
 end
