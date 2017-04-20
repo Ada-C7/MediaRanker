bb1f466feae550ac13a15d55a8f3ae52ad861768
Tamiko Terada <tamikodevo@gmail.com>
2017-04-19 21:55:52 -0700

    logout/login functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,12 +8,15 @@ class SessionsController < ApplicationController
 
     if user
       session[:user_id] = user.id
-      flash[:success] = "#{ user.username } is successfully logged in"
-      redirect_to root_path
-    # else
-    #   render :new
+      flash[:notice] = "#{ user.username } is successfully logged in"
+    else
+      new_user = User.new(username: params[:username])
+      flash[:notice] = "new user created!"
+      new_user.save
+      session[:user_id] = new_user.id
     end
-   end
+    redirect_to root_path
+  end
 
   def destroy
     session[:user_id] = nil
