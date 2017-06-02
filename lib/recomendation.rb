3f5e3f8164f49c77cd9177ba7af0029a6534787a
natalia-ku <bilberryfm@mail.ru>
2017-06-02 10:30:58 -0700

    jaccard index

---
+++ lib/recomendation.rb
@@ -0,0 +1,21 @@
+require 'jaccard'
+def give_recommendations(username, preferences, weights):
+  preference = preferences[username]
+  rank = Hash.new
+  preferences.each do |user_other,preference_other|
+    if username != user_other
+      similarity = jaccard_index(preference, preference_other)
+      if !similarity
+        next
+      end
+      preference_other.each do |movie|
+        if movie != preference
+          rank.setdefault(movie, 0)
+          rank[movie] += similarity
+        end
+      end
+    end
+  end
+
+## TODO:
+# in progress
